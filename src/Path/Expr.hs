617dcb0945c84e1e72d961debce3bcd825244739
Rob Rix <rob.rix@me.com>
2018-12-01 11:57:10 -0500

    Define evaluation.

--- src/Path/Expr.hs
+++ src/Path/Expr.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Path.Expr where
+
+import Control.Effect
+import Control.Effect.Fail
+import Control.Effect.Reader
+import Prelude hiding (fail)
 
 type Name = String
 
@@ -11,3 +17,15 @@ data Expr
 data Val
   = Closure Name Expr Env
   deriving (Eq, Ord, Show)
+
+type Env = [(Name, Val)]
+
+eval :: (Carrier sig m, Member (Reader Env) sig, MonadFail m) => Expr -> m Val
+eval (Var name) = do
+  val <- asks (lookup name)
+  maybe (fail ("free variable: " <> name)) pure val
+eval (Abs name body) = Closure name body <$> ask
+eval (App f a) = do
+  Closure n b e <- eval f
+  a' <- eval a
+  local (const ((n, a') : e)) (eval b)
