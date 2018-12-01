4da7aaf340a7c7ce4ca366ff74ac6d58a129d6f0
Rob Rix <rob.rix@me.com>
2018-12-01 13:17:44 -0500

    Define Expr as a functor, with Term as its fixpoint.

--- src/Path/Expr.hs
+++ src/Path/Expr.hs
@@ -8,33 +8,36 @@ import Prelude hiding (fail)
 
 type Name = String
 
-data Expr
+data Expr a
   = Var Name
-  | Abs Name Expr
-  | App Expr Expr
+  | Abs Name a
+  | App a a
+  deriving (Eq, Ord, Show)
+
+newtype Term = Term { unTerm :: Expr Term }
   deriving (Eq, Ord, Show)
 
 type Type = Expr
 
 data Val
-  = Closure Name Expr Env
+  = Closure Name Term Env
   deriving (Eq, Ord, Show)
 
 type Env = [(Name, Val)]
 
-eval :: (Carrier sig m, Member (Reader Env) sig, MonadFail m) => Expr -> m Val
-eval (Var name) = do
+eval :: (Carrier sig m, Member (Reader Env) sig, MonadFail m) => Term -> m Val
+eval (Term (Var name)) = do
   val <- asks (lookup name)
   maybe (fail ("free variable: " <> name)) pure val
-eval (Abs name body) = Closure name body <$> ask
-eval (App f a) = do
+eval (Term (Abs name body)) = Closure name body <$> ask
+eval (Term (App f a)) = do
   Closure n b e <- eval f
   a' <- eval a
   local (const ((n, a') : e)) (eval b)
 
 
-identity :: Expr
-identity = Abs "x" (Var "x")
+identity :: Term
+identity = Term (Abs "x" (Term (Var "x")))
 
-constant :: Expr
-constant = Abs "x" (Abs "y" (Var "x"))
+constant :: Term
+constant = Term (Abs "x" (Term (Abs "y" (Term (Var "x")))))
