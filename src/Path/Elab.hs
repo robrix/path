46ac92b9db791e7b24beb1828aac4127589d4e15
Rob Rix <rob.rix@me.com>
2018-12-02 17:30:09 -0500

    Define an Elaborate effect.

--- src/Path/Elab.hs
+++ src/Path/Elab.hs
@@ -1,1 +1,10 @@
+{-# LANGUAGE DeriveFunctor, KindSignatures #-}
 module Path.Elab where
+
+import Path.Expr
+
+data Elaborate (m :: * -> *) k
+  = Infer (Term Surface) (Elab -> k)
+  | Check (Term Surface) Type (Elab -> k)
+  | Define Elab k
+  deriving (Functor)
