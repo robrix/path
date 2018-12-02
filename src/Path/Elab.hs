a2bb45e29b2b4c3cd6a4db2a030de07337828230
Rob Rix <rob.rix@me.com>
2018-12-02 18:07:11 -0500

    Use a Reader effect for the binding level.

--- src/Path/Elab.hs
+++ src/Path/Elab.hs
@@ -26,32 +26,34 @@ instance Effect Elaborate where
 
 type Context = Map.Map Name Value
 
-infer :: (Carrier sig m, Member (Reader Context) sig, MonadFail m) => Int -> Term Surface -> m Elab
-infer i (Term (Ann e t)) = do
-  t' <- check i t VType
+infer :: (Carrier sig m, Member (Reader Context) sig, Member (Reader Int) sig, MonadFail m) => Term Surface -> m Elab
+infer (Term (Ann e t)) = do
+  t' <- check t VType
   let t'' = eval (erase t') []
-  check i e t''
-infer _ (Term (Core Type)) = pure (elab Type VType)
-infer i (Term (Core (Pi t b))) = do
-  t' <- check i t VType
+  check e t''
+infer (Term (Core Type)) = pure (elab Type VType)
+infer (Term (Core (Pi t b))) = do
+  t' <- check t VType
   let t'' = eval (erase t') []
-  b' <- local (Map.insert (Local i) t'') (check (succ i) (subst 0 (Term (Core (Free (Local i)))) b) VType)
+  i <- ask
+  b' <- local (Map.insert (Local i) t'') (local (const (succ i)) (check (subst 0 (Term (Core (Free (Local i)))) b) VType))
   pure (elab (Pi t' b') VType)
-infer _ (Term (Core (Free n))) = (asks (Map.lookup n)) >>= maybe (fail ("free variable: " <> show n)) (pure . elab (Free n))
-infer i (Term (Core (f :@ a))) = do
-  f' <- infer i f
+infer (Term (Core (Free n))) = (asks (Map.lookup n)) >>= maybe (fail ("free variable: " <> show n)) (pure . elab (Free n))
+infer (Term (Core (f :@ a))) = do
+  f' <- infer f
   case elabType f' of
     VPi t t' -> do
-      a' <- check i a t
+      a' <- check a t
       pure (elab (f' :@ a') (t' (eval (erase a') [])))
     _ -> fail ("illegal application of " <> show f')
-infer _ tm = fail ("no rule to infer type of " <> show tm)
+infer tm = fail ("no rule to infer type of " <> show tm)
 
-check :: (Carrier sig m, Member (Reader Context) sig, MonadFail m) => Int -> Term Surface -> Type -> m Elab
-check i (Term (Core (Lam e))) (VPi t t') = do
-  e' <- local (Map.insert (Local i) t) (check (succ i) (subst 0 (Term (Core (Free (Local i)))) e) (t' (vfree (Local i))))
+check :: (Carrier sig m, Member (Reader Context) sig, Member (Reader Int) sig, MonadFail m) => Term Surface -> Type -> m Elab
+check (Term (Core (Lam e))) (VPi t t') = do
+  i <- ask
+  e' <- local (Map.insert (Local i) t) (local (const (succ i)) (check (subst 0 (Term (Core (Free (Local i)))) e) (t' (vfree (Local i)))))
   pure (elab (Lam e') (VPi t t'))
-check i tm ty = do
-  v <- infer i tm
+check tm ty = do
+  v <- infer tm
   unless (elabType v == ty) (fail ("type mismatch: " <> show v <> " vs. " <> show ty))
   pure v
