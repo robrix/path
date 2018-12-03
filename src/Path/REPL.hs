fa4a463aa965ce934e833eedaf577518f7eda2e5
Rob Rix <rob.rix@me.com>
2018-12-02 19:29:12 -0500

    Define a REPL effect.

--- src/Path/REPL.hs
+++ src/Path/REPL.hs
@@ -1,1 +1,43 @@
+{-# LANGUAGE DeriveFunctor, FlexibleContexts, FlexibleInstances, KindSignatures, LambdaCase, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
 module Path.REPL where
+
+import Control.Effect
+import Control.Effect.Carrier
+import Control.Effect.Sum
+import Control.Monad.IO.Class
+import Data.Coerce
+import System.Console.Haskeline
+
+data REPL (m :: * -> *) k
+  = Prompt String (Maybe String -> k)
+  | Output String k
+  deriving (Functor)
+
+instance HFunctor REPL where
+  hmap _ = coerce
+
+instance Effect REPL where
+  handle state handler = coerce . fmap (handler . (<$ state))
+
+prompt :: (Member REPL sig, Carrier sig m) => String -> m (Maybe String)
+prompt p = send (Prompt p ret)
+
+output :: (Member REPL sig, Carrier sig m) => String -> m ()
+output s = send (Output s (ret ()))
+
+runREPL :: (MonadIO m, Carrier sig m) => Prefs -> Settings IO -> Eff (REPLC m) a -> m a
+runREPL prefs settings = flip runREPLC (prefs, settings) . interpret
+
+newtype REPLC m a = REPLC { runREPLC :: (Prefs, Settings IO) -> m a }
+
+instance (Carrier sig m, MonadIO m) => Carrier (REPL :+: sig) (REPLC m) where
+  ret = REPLC . const . ret
+  eff op = REPLC (\ args -> handleSum (eff . handleReader args runREPLC) (\case
+    Prompt p k -> liftIO (uncurry runInputTWithPrefs args (getInputLine (cyan <> p <> plain))) >>= flip runREPLC args . k
+    Output s k -> liftIO (uncurry runInputTWithPrefs args (outputStrLn s)) *> runREPLC k args) op)
+
+cyan :: String
+cyan = "\ESC[1;36m\STX"
+
+plain :: String
+plain = "\ESC[0m\STX"
