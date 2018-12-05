25df262f48bf357151686937d353ca3059e7a1a8
Rob Rix <rob.rix@me.com>
2018-12-04 21:14:25 -0500

    Define a PrettyPrec instance for Core.

--- src/Path/Core.hs
+++ src/Path/Core.hs
@@ -3,8 +3,10 @@ module Path.Core where
 
 import Data.Function (fix)
 import qualified Data.Set as Set
+import Data.Text.Prettyprint.Doc
 import Path.FreeVariables
 import Path.Name
+import Path.Pretty
 import Path.Plicity
 import Path.Term
 
@@ -16,6 +18,19 @@ data Core a
   | Type
   | Pi String Plicity a a
   deriving (Eq, Functor, Ord, Show)
+
+instance (FreeVariables a, PrettyPrec a) => PrettyPrec (Core a) where
+  prettyPrec d c = case c of
+    Bound s -> pretty s
+    Free (Global s) -> pretty s
+    Free (Local s) -> pretty s
+    Free (Quote s) -> pretty '\'' <> pretty s
+    Lam v b -> prettyParens (d > 0) $ pretty "\\ " <> pretty v <> pretty " -> " <> prettyPrec 0 b
+    f :@ a -> prettyParens (d > 10) $ prettyPrec 10 f <> pretty ' ' <> prettyPrec 11 a
+    Type -> pretty "Type"
+    Pi v _ t b
+      | Set.member (Local v) (fvs b) -> prettyParens (d > 1) $ prettyBraces True (pretty v <> pretty " : " <> prettyPrec 0 t) <> pretty " -> " <> prettyPrec 1 b
+      | otherwise -> prettyPrec 2 t <> pretty " -> " <> prettyPrec 1 b
 
 instance Show (Term Core) where
   showsPrec = fix (\ f d (Term core) -> showCore f fvs d core)
