139540ac51eee18170132dfc150b2ffef6247c06
Rob Rix <rob.rix@me.com>
2018-12-04 20:55:15 -0500

    Define a FreeVariables instance for Term.

--- src/Path/Term.hs
+++ src/Path/Term.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, StandaloneDeriving, UndecidableInstances #-}
 module Path.Term where
 
+import Path.FreeVariables
 import Path.Recursive
 
 newtype Term f = Term (f (Term f))
 
 deriving instance Eq (f (Term f)) => Eq (Term f)
 deriving instance Ord (f (Term f)) => Ord (Term f)
+
+instance (FreeVariables1 f, Functor f) => FreeVariables (Term f) where
+  fvs = cata (liftFvs id)
 
 instance Functor f => Recursive f (Term f) where project (Term f) = f
