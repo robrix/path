850f3192fbbd800b843d503c0e97dc1ae6438c6f
Rob Rix <rob.rix@me.com>
2018-12-04 20:44:54 -0500

    Add some conveniences for printing Docs to a terminal.

--- src/Path/Pretty.hs
+++ src/Path/Pretty.hs
@@ -1,1 +1,16 @@
 module Path.Pretty where
+
+import Data.Text.Prettyprint.Doc
+import Data.Text.Prettyprint.Doc.Render.Terminal
+import System.Console.Terminal.Size as Size
+import System.IO (stdout)
+
+putDoc :: Doc AnsiStyle -> IO ()
+putDoc doc = do
+  options <- layoutOptions
+  renderIO stdout (layoutPretty options (doc <> line))
+
+layoutOptions :: IO LayoutOptions
+layoutOptions = do
+  s <- maybe 80 Size.width <$> size
+  pure LayoutOptions { layoutPageWidth = AvailablePerLine s 1 }
