5642e2566afbb6af0cb2c0a97c28f4e1835f44d9
Rob Rix <rob.rix@me.com>
2018-12-04 21:43:18 -0500

    Define a Pretty instance for Term.

--- src/Path/Term.hs
+++ src/Path/Term.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, StandaloneDeriving, UndecidableInstances #-}
 module Path.Term where
 
+import Data.Text.Prettyprint.Doc
 import Path.FreeVariables
 import Path.Pretty
 import Path.Recursive
@@ -11,6 +12,9 @@ deriving instance Eq (f (Term f)) => Eq (Term f)
 deriving instance Ord (f (Term f)) => Ord (Term f)
 deriving instance Show (f (Term f)) => Show (Term f)
 deriving instance PrettyPrec (f (Term f)) => PrettyPrec (Term f)
+
+instance PrettyPrec (f (Term f)) => Pretty (Term f) where
+  pretty = prettyPrec 0
 
 instance (FreeVariables1 f, Functor f) => FreeVariables (Term f) where
   fvs = cata (liftFvs id)
