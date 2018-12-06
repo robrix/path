ee6bfb6519563f7f2b0f786d37a90f8632833687
Rob Rix <rob.rix@me.com>
2018-12-05 20:57:19 -0500

    Define a Pretty instance for Name.

--- src/Path/Name.hs
+++ src/Path/Name.hs
@@ -1,7 +1,14 @@
 module Path.Name where
 
+import Data.Text.Prettyprint.Doc
+
 data Name
   = Global String
   | Local String
   | Quote String
   deriving (Eq, Ord, Show)
+
+instance Pretty Name where
+  pretty (Global s) = pretty s
+  pretty (Local s) = pretty s
+  pretty (Quote s) = pretty s
