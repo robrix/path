6807f89fcc72f371dc7244da0c33e6719d5d7d59
Rob Rix <rob.rix@me.com>
2018-12-05 20:21:39 -0500

    Define infer & check in terms of a single elab function.

--- src/Path/Elab.hs
+++ src/Path/Elab.hs
@@ -18,21 +18,21 @@ type Type = Value
 
 type Context = Map.Map Name Value
 
-infer :: (Carrier sig m, Member (Reader Context) sig, Member (Reader Env) sig, MonadFail m) => Term Surface -> m (Ann Core Type)
-infer (Term (e ::: t)) = do
+elab :: (Carrier sig m, Member (Reader Context) sig, Member (Reader Env) sig, MonadFail m) => Term Surface -> Maybe Type -> m (Ann Core Type)
+elab (Term (e ::: t)) Nothing = do
   t' <- check t VType
   env <- ask
   let t'' = eval (erase t') env
   check e t''
-infer (Term (Core Type)) = pure (Ann Type VType)
-infer (Term (Core (Pi n e t b))) = do
+elab (Term (Core Type)) Nothing = pure (Ann Type VType)
+elab (Term (Core (Pi n e t b))) Nothing = do
   t' <- check t VType
   env <- ask
   let t'' = eval (erase t') env
   b' <- local (Map.insert (Local n) t'') (check (subst n (Term (Core (Free (Local n)))) b) VType)
   pure (Ann (Pi n e t' b') VType)
-infer (Term (Core (Free n))) = asks (Map.lookup n) >>= maybe (fail ("free variable: " <> show n)) (pure . Ann (Free n))
-infer (Term (Core (f :@ a))) = do
+elab (Term (Core (Free n))) Nothing = asks (Map.lookup n) >>= maybe (fail ("free variable: " <> show n)) (pure . Ann (Free n))
+elab (Term (Core (f :@ a))) Nothing = do
   f' <- infer f
   case ann f' of
     VPi _ _ t t' -> do
@@ -40,13 +40,17 @@ infer (Term (Core (f :@ a))) = do
       env <- ask
       pure (Ann (f' :@ a') (t' (eval (erase a') env)))
     _ -> fail ("illegal application of " <> show f')
-infer tm = fail ("no rule to infer type of " <> show tm)
-
-check :: (Carrier sig m, Member (Reader Context) sig, Member (Reader Env) sig, MonadFail m) => Term Surface -> Type -> m (Ann Core Type)
-check (Term (Core (Lam n e))) (VPi tn ann t t') = do
+elab tm Nothing = fail ("no rule to infer type of " <> show tm)
+elab (Term (Core (Lam n e))) (Just (VPi tn ann t t')) = do
   e' <- local (Map.insert (Local n) t) (check (subst n (Term (Core (Free (Local n)))) e) (t' (vfree (Local n))))
   pure (Ann (Lam n e') (VPi tn ann t t'))
-check tm ty = do
+elab tm (Just ty) = do
   v <- infer tm
   unless (ann v == ty) (fail ("type mismatch: " <> show v <> " vs. " <> show ty))
   pure v
+
+infer :: (Carrier sig m, Member (Reader Context) sig, Member (Reader Env) sig, MonadFail m) => Term Surface -> m (Ann Core Type)
+infer tm = elab tm Nothing
+
+check :: (Carrier sig m, Member (Reader Context) sig, Member (Reader Env) sig, MonadFail m) => Term Surface -> Type -> m (Ann Core Type)
+check tm = elab tm . Just
