89ac0099113ba0d703cc5490d8675141de978a7a
Rob Rix <rob.rix@me.com>
2018-12-05 20:17:33 -0500

    :fire: Path.Recursive.

--- src/Path/Recursive.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE FunctionalDependencies #-}
-module Path.Recursive where
-
-class Functor f => Recursive f t | t -> f where
-  project :: t -> f t
-
-cata :: Recursive f t => (f a -> a) -> t -> a
-cata algebra = go
-  where go = algebra . fmap go . project
