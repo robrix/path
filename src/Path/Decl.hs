0e44946c10e7d170169e49dd25ab1ad520a93d90
Rob Rix <rob.rix@me.com>
2018-12-06 21:23:37 -0500

    Compute load orders from module graphs.

--- src/Path/Decl.hs
+++ src/Path/Decl.hs
@@ -6,11 +6,14 @@ import Control.Effect.Carrier
 import Control.Effect.Error
 import Control.Effect.NonDet
 import Control.Effect.Reader
+import Control.Effect.State
+import Control.Monad ((<=<), when)
 import Data.List.NonEmpty (NonEmpty)
 import qualified Data.Map as Map
 import Data.Maybe (fromMaybe)
 import Data.Monoid (Alt(..))
 import qualified Data.Set as Set
+import Data.Traversable (for)
 import Path.Surface
 import Path.Term
 
@@ -55,3 +58,18 @@ data ModuleError
   = UnknownModule ModuleName
   | CyclicImport [ModuleName]
   deriving (Eq, Ord, Show)
+
+
+loadOrder :: ModuleGraph -> Either ModuleError [Module]
+loadOrder g = concat <$> run (runError (evalState (Set.empty :: Set.Set ModuleName) (runReader (Set.empty :: Set.Set ModuleName) (traverse loop (Map.elems (unModuleGraph g))))))
+  where loop m = do
+          inPath <- asks (Set.member (moduleName m))
+          when inPath (cycleFrom (moduleName m) g)
+          visited <- gets (Set.member (moduleName m))
+          if visited then
+            pure []
+          else
+            local (Set.insert (moduleName m)) $ do
+              ms <- for (moduleImports m) (loop <=< flip lookupModule g . importModuleName)
+              modify (Set.insert (moduleName m))
+              pure (m : concat ms)
