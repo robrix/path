3e409b35b8ca9c0592aeb9b92e7b84b50bd7b6e9
Rob Rix <rob.rix@me.com>
2018-12-06 21:45:04 -0500

    Correct the definition of loadOrder.

--- src/Path/Module.hs
+++ src/Path/Module.hs
@@ -7,13 +7,13 @@ import Control.Effect.Error
 import Control.Effect.NonDet
 import Control.Effect.Reader
 import Control.Effect.State
-import Control.Monad (when)
+import Control.Monad (unless, when)
+import Data.Foldable (for_)
 import Data.List.NonEmpty (NonEmpty)
 import qualified Data.Map as Map
 import Data.Maybe (fromMaybe)
 import Data.Monoid (Alt(..))
 import qualified Data.Set as Set
-import Data.Traversable (for)
 import Path.Decl
 
 type ModuleName = NonEmpty String
@@ -55,16 +55,13 @@ data ModuleError
 
 
 loadOrder :: ModuleGraph -> Either ModuleError [Module]
-loadOrder g = concat <$> run (runError (evalState (Set.empty :: Set.Set ModuleName) (runReader g (runReader (Set.empty :: Set.Set ModuleName) (for (Map.keys (unModuleGraph g)) loop)))))
+loadOrder g = reverse <$> run (runError (execState [] (evalState (Set.empty :: Set.Set ModuleName) (runReader g (runReader (Set.empty :: Set.Set ModuleName) (for_ (Map.keys (unModuleGraph g)) loop))))))
   where loop n = do
           inPath <- asks (Set.member n)
           when inPath (cycleFrom n)
           visited <- gets (Set.member n)
-          if visited then
-            pure []
-          else
-            local (Set.insert n) $ do
-              m <- lookupModule n
-              ms <- for (moduleImports m) (loop . importModuleName)
-              modify (Set.insert n)
-              pure (m : concat ms)
+          unless visited . local (Set.insert n) $ do
+            m <- lookupModule n
+            for_ (moduleImports m) (loop . importModuleName)
+            modify (Set.insert n)
+            modify (m :)
