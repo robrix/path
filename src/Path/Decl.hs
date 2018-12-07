0808978c18d2dbe71822e84501cf3d59f6abe12b
Rob Rix <rob.rix@me.com>
2018-12-06 20:02:58 -0500

    Define a function to compute cyclic imports.

--- src/Path/Decl.hs
+++ src/Path/Decl.hs
@@ -2,6 +2,8 @@ module Path.Decl where
 
 import Data.List.NonEmpty
 import qualified Data.Map as Map
+import Data.Monoid (First(..))
+import qualified Data.Set as Set
 import Path.Surface
 import Path.Term
 
@@ -31,3 +33,11 @@ moduleGraph = ModuleGraph . Map.fromList . map ((,) . moduleName <*> id)
 
 lookupModule :: ModuleName -> ModuleGraph -> Maybe Module
 lookupModule name = Map.lookup name . unModuleGraph
+
+cycleFrom :: ModuleName -> ModuleGraph -> Maybe [ModuleName]
+cycleFrom m g = go Set.empty m
+  where go v n
+          | n `Set.member` v = Just [n]
+          | otherwise        = do
+            m <- lookupModule n g
+            (n :) <$> getFirst (foldMap (First . go (Set.insert (moduleName m) v) . importModuleName) (moduleImports m))
