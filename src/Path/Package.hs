1dfe0985ba9235ea0c0655bc959968ae37c13429
Rob Rix <rob.rix@me.com>
2018-12-06 22:14:38 -0500

    Packages have targets.

--- src/Path/Package.hs
+++ src/Path/Package.hs
@@ -4,4 +4,19 @@ import Path.Module
 
 data Package = Package
   { packageName    :: String
+  , packageTargets :: [Target]
   }
+
+data Target
+  = Library
+    { targetName        :: String
+    , targetModuleNames :: [ModuleName]
+    }
+  | Executable
+    { targetName        :: String
+    , targetModuleNames :: [ModuleName]
+    }
+  | Test
+    { targetName        :: String
+    , targetModuleNames :: [ModuleName]
+    }
