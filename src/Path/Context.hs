2829cff8589048915d55bd70f9c07261c639d11f
Rob Rix <rob.rix@me.com>
2018-12-08 14:36:13 -0500

    Add contexts.

--- src/Path/Context.hs
+++ src/Path/Context.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 module Path.Context where
 
-import Data.Bifunctor (first)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
+import Data.Tuple (swap)
 import Path.Eval
 import Path.Name
 import Path.Semiring
@@ -12,17 +12,18 @@ import Text.PrettyPrint.ANSI.Leijen hiding ((<$>))
 
 type Type = Value
 
-newtype Context = Context { unContext :: Map.Map Name (Set.Set (Usage, Type)) }
+newtype Context = Context { unContext :: Map.Map Name (Map.Map Type Usage) }
   deriving (Eq, Ord, Show)
 
 empty :: Context
 empty = Context Map.empty
 
 lookup :: Name -> Context -> Maybe (Set.Set (Usage, Type))
-lookup n = Map.lookup n . unContext
+lookup n = fmap toSet . Map.lookup n . unContext
+  where toSet = Set.fromList . map swap . Map.assocs
 
 insert :: Name -> (Usage, Type) -> Context -> Context
-insert n v = Context . Map.insertWith (<>) n (Set.singleton v) . unContext
+insert n (u, t) = Context . Map.insertWith (<>) n (Map.singleton t u) . unContext
 
 union :: Context -> Context -> Context
 union (Context c1) (Context c2) = Context (Map.unionWith (<>) c1 c2)
@@ -33,11 +34,14 @@ disambiguate tys | [ty] <- Set.toList tys = Just ty
 
 instance Pretty Context where
   pretty = vsep . map prettyBindings . Map.toList . unContext
-    where prettyBindings (name, tys) = vsep (map (prettyBinding name) (Set.toList tys))
-          prettyBinding name (u, ty) = pretty name <+> pretty ":" <> prettyUsage u <+> group (pretty ty)
+    where prettyBindings (name, tys) = vsep (map (prettyBinding name) (Map.toList tys))
+          prettyBinding name (ty, u) = pretty name <+> pretty ":" <> prettyUsage u <+> group (pretty ty)
           prettyUsage Zero = pretty "⁰"
           prettyUsage One  = pretty "¹"
           prettyUsage More = pretty "ⁿ"
 
+instance Semigroup Context where
+  Context c1 <> Context c2 = Context (Map.unionWith (Map.unionWith (<>)) c1 c2)
+
 instance LeftModule Usage Context where
-  u ><< Context c = Context (Set.map (first (u ><)) <$> c)
+  u ><< Context c = Context (fmap (u ><) <$> c)
