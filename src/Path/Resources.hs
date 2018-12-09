d04ac2bf618e247bdb0e903da02fc6d9adecef40
Rob Rix <rob.rix@me.com>
2018-12-09 17:26:06 -0500

    Define Resources as a newtype.

--- src/Path/Resources.hs
+++ src/Path/Resources.hs
@@ -1,16 +1,29 @@
 module Path.Resources where
 
 import qualified Data.Map as Map
+import Path.FreeVariables
 import Path.Name
+import Path.Pretty
 import Path.Usage
+import Text.PrettyPrint.ANSI.Leijen
 
-type Resources = Map.Map Name Usage
+newtype Resources = Resources { unResources :: Map.Map Name Usage }
+  deriving (Eq, Ord, Show)
 
 empty :: Resources
-empty = Map.empty
+empty = Resources Map.empty
 
 singleton :: Name -> Usage -> Resources
-singleton n = Map.singleton n
+singleton n = Resources . Map.singleton n
 
 delete :: Name -> Resources -> Resources
-delete n = Map.delete n
+delete n = Resources . Map.delete n . unResources
+
+instance Pretty Resources where
+  pretty = vsep . map (uncurry prettyBinding) . Map.toList . unResources
+    where prettyBinding name u = pretty name <+> pretty "@" <+> pretty u
+
+instance PrettyPrec Resources
+
+instance FreeVariables Resources where
+  fvs = fvs . unResources
