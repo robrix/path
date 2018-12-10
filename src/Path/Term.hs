cf14083b5b61344a9e7bb651f703b04de341c9b2
Rob Rix <rob.rix@me.com>
2018-12-10 09:00:16 -0500

    Revert "Define a catamorphism over terms."
    This reverts commit 3d4c826e77eb6b74d75577faed693e6ba508a8c8.

--- src/Path/Term.hs
+++ src/Path/Term.hs
@@ -22,6 +22,3 @@ instance (FreeVariables1 f, FreeVariables a) => FreeVariables (Term f a) where
 
 instance Functor f => Functor (Term f) where
   fmap f (In tm a) = In (fmap (fmap f) tm) (f a)
-
-cata :: Functor f => (f b -> a -> b) -> Term f a -> b
-cata alg = go where go = alg . fmap go . out <*> ann
