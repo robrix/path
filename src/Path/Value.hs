b950a3e7b069b64dd5ae26dccef0aeeb2528efcc
Rob Rix <rob.rix@me.com>
2018-12-13 15:12:56 -0500

    Parameterize quote by a function determining the name.

--- src/Path/Value.hs
+++ src/Path/Value.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances, LambdaCase, MultiParamTypeClasses #-}
 module Path.Value where
 
 import Data.Function (on)
@@ -16,22 +16,22 @@ data Value v
   | VNeutral (Neutral v)
 
 instance Ord v => Eq (Value v) where
-  (==) = aeq `on` quote
+  (==) = aeq `on` quote (flip const)
 
 instance Ord v => Ord (Value v) where
-  compare = compare `on` quote
+  compare = compare `on` quote (flip const)
 
 instance Show v => Show (Value v) where
-  showsPrec d = showsPrec d . quote
+  showsPrec d = showsPrec d . quote (flip const)
 
 instance (Ord v, Pretty v) => PrettyPrec (Value v) where
-  prettyPrec d = prettyPrec d . quote
+  prettyPrec d = prettyPrec d . quote (flip const)
 
 instance (Ord v, Pretty v) => Pretty (Value v) where
   pretty = prettyPrec 0
 
 instance Ord v => FreeVariables v (Value v) where
-  fvs = fvs . quote
+  fvs = fvs . quote (flip const)
 
 vfree :: v -> Value v
 vfree = VNeutral . NFree
@@ -40,12 +40,13 @@ data Neutral v
   = NFree v
   | NApp (Neutral v) (Value v)
 
-quote :: Value v -> Term (Core v) ()
-quote VType = In Type ()
-quote (VLam n f) = In (Lam n (quote (f (vfree (fromMaybe undefined n))))) ()
-quote (VPi n e t f) = In (Pi n e (quote t) (quote (f (vfree (fromMaybe undefined n))))) ()
-quote (VNeutral n) = quoteN n
+quote :: (Int -> v -> u) -> Value v -> Term (Core u) ()
+quote f = go 0
+  where go i = \case
+          VType -> In Type ()
+          VLam n b -> In (Lam (f i <$> n) (go (succ i) (b (vfree (fromMaybe undefined n))))) ()
+          VPi n e t b -> In (Pi (f i <$> n) e (go i t) (go (succ i) (b (vfree (fromMaybe undefined n))))) ()
+          VNeutral n -> goN i n
 
-quoteN :: Neutral v -> Term (Core v) ()
-quoteN (NFree n) = In (Var n) ()
-quoteN (NApp n a) = In (quoteN n :@ quote a) ()
+        goN i (NFree n) = In (Var (f i n)) ()
+        goN i (NApp n a) = In (goN i n :@ go i a) ()
