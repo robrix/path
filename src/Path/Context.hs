f8a3a6d8a54dad5fe479cb42188e4d49e60abd94
Rob Rix <rob.rix@me.com>
2018-12-12 19:20:46 -0500

    Don’t store multiple values in contexts.

--- src/Path/Context.hs
+++ src/Path/Context.hs
@@ -1,9 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 module Path.Context where
 
-import Control.Monad ((<=<))
 import qualified Data.Map as Map
-import qualified Data.Set as Set
 import Path.Eval
 import Path.Name
 import Path.Pretty
@@ -11,31 +9,26 @@ import Text.PrettyPrint.ANSI.Leijen hiding ((<$>))
 
 type Type = Value
 
-newtype Context = Context { unContext :: Map.Map Name (Set.Set Type) }
+newtype Context = Context { unContext :: Map.Map Name Type }
   deriving (Eq, Ord, Show)
 
 empty :: Context
 empty = Context Map.empty
 
 lookup :: Name -> Context -> Maybe Type
-lookup n = disambiguate <=< Map.lookup n . unContext
+lookup n = Map.lookup n . unContext
 
 insert :: Name -> Type -> Context -> Context
-insert n t = Context . Map.insertWith (<>) n (Set.singleton t) . unContext
+insert n t = Context . Map.insert n t . unContext
 
 union :: Context -> Context -> Context
-union (Context c1) (Context c2) = Context (Map.unionWith (<>) c1 c2)
-
-disambiguate :: Set.Set a -> Maybe a
-disambiguate tys | [ty] <- Set.toList tys = Just ty
-                 | otherwise              = Nothing
+union (Context c1) (Context c2) = Context (Map.union c1 c2)
 
 instance Pretty Context where
-  pretty = vsep . map prettyBindings . Map.toList . unContext
-    where prettyBindings (name, tys) = vsep (map (prettyBinding name) (Set.toList tys))
-          prettyBinding name ty = pretty name <+> pretty ":" <+> group (pretty ty)
+  pretty = vsep . map (uncurry prettyBinding) . Map.toList . unContext
+    where prettyBinding name ty = pretty name <+> pretty ":" <+> group (pretty ty)
 
 instance PrettyPrec Context
 
 instance Semigroup Context where
-  Context c1 <> Context c2 = Context (Map.unionWith (<>) c1 c2)
+  Context c1 <> Context c2 = Context (Map.union c1 c2)
