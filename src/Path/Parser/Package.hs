86bcbf36ea3b90c04469b6a5b6e60ecc10ef51ec
Rob Rix <rob.rix@me.com>
2018-12-16 17:42:50 -0500

    Fields’ values are indentation-sensitive.

--- src/Path/Parser/Package.hs
+++ src/Path/Parser/Package.hs
@@ -4,9 +4,10 @@ import Control.Applicative (Alternative(..))
 import Path.Name
 import Text.Parser.Token.Highlight
 import Text.Trifecta
+import Text.Trifecta.Indentation
 
 packageName :: (Monad m, TokenParsing m) => m PackageName
 packageName = ident (IdentifierStyle "package name" letter (alphaNum <|> oneOf "-'_") mempty Identifier ReservedIdentifier)
 
-field :: (Monad m, TokenParsing m) => String -> m a -> m a
-field name m = token (string name) *> colon *> m
+field :: (IndentationParsing m, Monad m, TokenParsing m) => String -> m a -> m a
+field name m = token (string name) *> colon *> localIndentation Gt m
