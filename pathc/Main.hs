8a7d6e00ee2b71dd7038dad621563dd60627b809
Rob Rix <rob.rix@me.com>
2018-12-16 16:44:17 -0500

    Move the options parser into Path.CLI.

---
+++ pathc/Main.hs
@@ -0,0 +1,4 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module Main (main) where
+
+import Path.CLI (main)
--- src/Path/CLI.hs
+++ src/Path/CLI.hs
@@ -1,1 +1,24 @@
 module Path.CLI where
+
+import Data.Version (showVersion)
+import Options.Applicative as Options
+import qualified Paths_path as Library (version)
+
+main :: IO ()
+main = execParser argumentsParser
+
+argumentsParser :: ParserInfo ()
+argumentsParser = info
+  (version <*> helper <*> options)
+  (  fullDesc
+  <> progDesc "Path is a small experiment in quantitative type theory."
+  <> header   "Path - a quantitative, dependently-typed language")
+
+options :: Parser ()
+options = pure ()
+
+versionString :: String
+versionString = "pathc version " <> showVersion Library.version
+
+version :: Options.Parser (a -> a)
+version = infoOption versionString (long "version" <> short 'V' <> help "Output version info.")
