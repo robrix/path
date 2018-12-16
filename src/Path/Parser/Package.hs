ff26309e93f7310214411169b535bb7feb42de98
Rob Rix <rob.rix@me.com>
2018-12-16 17:36:38 -0500

    Define a parser for package names.

--- src/Path/Parser/Package.hs
+++ src/Path/Parser/Package.hs
@@ -1,1 +1,9 @@
 module Path.Parser.Package where
+
+import Control.Applicative (Alternative(..))
+import Path.Name
+import Text.Parser.Token.Highlight
+import Text.Trifecta
+
+packageName :: (Monad m, TokenParsing m) => m PackageName
+packageName = ident (IdentifierStyle "package name" letter (alphaNum <|> oneOf "-'_") mempty Identifier ReservedIdentifier)
