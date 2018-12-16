a216773c8d37f4795125b5733ff6e055a41b772a
Rob Rix <rob.rix@me.com>
2018-12-16 16:32:22 -0500

    Parse basic version options.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,21 @@
 module Main where
 
+import Data.Version (showVersion)
+import Options.Applicative as Options
+import qualified Paths_path as Library (version)
+
 main :: IO ()
-main = pure ()
+main = execParser argumentsParser
+
+argumentsParser :: ParserInfo ()
+argumentsParser = info
+  (version <*> helper <*> pure ())
+  (  fullDesc
+  <> progDesc "Path is a small experiment in quantitative type theory."
+  <> header   "Path - a quantitative, dependently-typed language")
+
+versionString :: String
+versionString = "pathc version " <> showVersion Library.version
+
+version :: Options.Parser (a -> a)
+version = infoOption versionString (long "version" <> short 'V' <> help "Output version info.")
