f0a1d026e52a3c42433e81e0a761664885902105
Rob Rix <rob.rix@me.com>
2018-12-16 20:36:10 -0500

    :fire: some'.

--- src/Path/Parser/Package.hs
+++ src/Path/Parser/Package.hs
@@ -5,14 +5,13 @@ import Path.Name
 import Path.Package
 import Text.Parser.Token.Highlight
 import Text.Trifecta
-import Text.Trifecta.Indentation
 
-package :: (IndentationParsing m, Monad m, TokenParsing m) => m Package
+package :: (Monad m, TokenParsing m) => m Package
 package
   =   Package
   <$> field "name" packageName'
   <*> pure []
-  <*> field "sources" (some' filePath)
+  <*> field "sources" (commaSep1 filePath)
 
 packageName' :: (Monad m, TokenParsing m) => m PackageName
 packageName' = ident (IdentifierStyle "package name" letter (alphaNum <|> oneOf "-'_") mempty Identifier ReservedIdentifier)
@@ -20,8 +19,5 @@ packageName' = ident (IdentifierStyle "package name" letter (alphaNum <|> oneOf 
 filePath :: TokenParsing m => m FilePath
 filePath = token ((alphaNum <|> char '.') `sepBy1` char '/')
 
-some' :: (IndentationParsing m, TokenParsing m) => m a -> m [a]
-some' m = commaSep1 (absoluteIndentation m)
-
-field :: (IndentationParsing m, Monad m, TokenParsing m) => String -> m a -> m a
-field name m = token (string name) *> colon *> localIndentation Gt m
+field :: (Monad m, TokenParsing m) => String -> m a -> m a
+field name m = token (string name) *> colon *> m
