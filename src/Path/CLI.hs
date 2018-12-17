74c59331606a603739b9a68358410fe2caafd85d
Rob Rix <rob.rix@me.com>
2018-12-17 10:51:47 -0500

    Require parsing the whole file.

--- src/Path/CLI.hs
+++ src/Path/CLI.hs
@@ -6,7 +6,7 @@ import Control.Monad (join)
 import Data.Version (showVersion)
 import Options.Applicative as Options
 import Path.Package
-import Path.Parser (parseFile)
+import Path.Parser (parseFile, whole)
 import Path.Parser.Package as Parser (package)
 import Path.REPL
 import qualified Paths_path as Library (version)
@@ -25,7 +25,7 @@ options :: Parser (IO ())
 options
   =   flag' (either printParserError repl =<<) (short 'i' <> long "interactive" <> help "run interactively")
   <*> (pure . Right <$> some source <|> parsePackage <$> strOption (long "package-path" <> metavar "FILE" <> help "source file"))
-  where parsePackage = fmap (fmap packageSources) . runM . runError . parseFile Parser.package
+  where parsePackage = fmap (fmap packageSources) . runM . runError . parseFile (whole Parser.package)
 
 
 constraint :: Parser Constraint
