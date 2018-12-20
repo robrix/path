26b726cb5e7a8a20de93b25125c5e2d05460e972
Rob Rix <rob.rix@me.com>
2018-12-20 13:42:01 -0500

    Revert "Stub in syntax for modules."
    This reverts commit a2be97295f47535c668dfb91b9ab02ff063db50b.

--- src/Path/Meta.hs
+++ src/Path/Meta.hs
@@ -3,9 +3,4 @@ module Path.Meta where
 
 import Path.Name
 
-data Module v f = Module
-  { moduleName    :: ModuleName
-  , moduleImports :: [f Import]
-  }
-
 data Import (f :: (* -> *) -> * -> *) = Import { importModuleName :: ModuleName }
