23df1d7d8fe25630f2e6f501d39b65d5f6dcb76f
Rob Rix <rob.rix@me.com>
2018-12-20 12:26:13 -0500

    Generalize vapp, vforce, and vforceN.

--- src/Path/Eval.hs
+++ src/Path/Eval.hs
@@ -17,19 +17,19 @@ eval (In (f :@ a) _) d = eval f d `vapp` eval a d
 eval (In Type _) _ = VType
 eval (In (Pi n e ty b) _) d = VPi n e (eval ty d) (eval b . flip (Env.insert n) d)
 
-vapp :: Value QName -> Value QName -> Value QName
+vapp :: Show v => Value v -> Value v -> Value v
 vapp (VLam _ f) v = f v
 vapp (VNeutral n) v = VNeutral (NApp n v)
 vapp f a = error ("illegal application of " <> show f <> " to " <> show a)
 
-vforce :: Env QName -> Value QName -> Value QName
+vforce :: (Ord v, Show v) => Env v -> Value v -> Value v
 vforce d = \case
   VLam v f    -> VLam v (vforce d . f)
   VType       -> VType
   VPi v u t b -> VPi v u (vforce d t) (vforce d . b)
   VNeutral n  -> vforceN d n
 
-vforceN :: Env QName -> Neutral QName -> Value QName
+vforceN :: (Ord v, Show v) => Env v -> Neutral v -> Value v
 vforceN d = \case
   NApp f a -> vforceN d f `vapp` vforce d a
   NFree v  -> maybe (vfree v) (vforce d) (Env.lookup v d)
