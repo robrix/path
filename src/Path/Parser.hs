ca6327cbfcea6c00ac8cf08d9b27d9657e3d092b
Rob Rix <rob.rix@me.com>
2018-12-20 15:06:40 -0500

    Add a ReaderT to Parser to model the current layout mode.

--- src/Path/Parser.hs
+++ src/Path/Parser.hs
@@ -20,6 +20,7 @@ import Control.Effect
 import Control.Effect.Error
 import Control.Monad (MonadPlus(..), (<=<))
 import Control.Monad.IO.Class
+import Control.Monad.Reader
 import Control.Monad.State
 import Data.Char (isPunctuation, isSpace, isSymbol)
 import qualified Data.HashSet as HashSet
@@ -36,7 +37,7 @@ import Text.Trifecta.Delta
 data Layout = Indent | Braces
   deriving (Eq, Ord, Show)
 
-newtype Parser a = Parser { runParser :: StateT Int Trifecta.Parser a }
+newtype Parser a = Parser { runParser :: StateT Int (ReaderT Layout Trifecta.Parser) a }
   deriving (Alternative, Applicative, CharParsing, DeltaParsing, Functor, LookAheadParsing, MarkParsing Delta, Monad, MonadPlus, Parsing)
 
 instance TokenParsing Parser where
@@ -47,10 +48,10 @@ instance TokenParsing Parser where
 
 
 parseFile :: (Carrier sig m, Member (Error ErrInfo) sig, MonadIO m) => Parser a -> FilePath -> m a
-parseFile p = toError <=< parseFromFileEx (evalStateT (runParser p) 0)
+parseFile p = toError <=< parseFromFileEx (runReaderT (evalStateT (runParser p) 0) Indent)
 
 parseString :: (Carrier sig m, Member (Error ErrInfo) sig, MonadIO m) => Parser a -> String -> m a
-parseString p = toError . Trifecta.parseString (evalStateT (runParser p) 0) mempty
+parseString p = toError . Trifecta.parseString (runReaderT (evalStateT (runParser p) 0) Indent) mempty
 
 toError :: (Applicative m, Carrier sig m, Member (Error ErrInfo) sig) => Result a -> m a
 toError (Success a) = pure a
