cb0858a35314f7bd0fd775e822ef36da9917de90
Rob Rix <rob.rix@me.com>
2018-12-20 13:07:02 -0500

    Stub in syntax for declarations.

--- src/Path/Meta.hs
+++ src/Path/Meta.hs
@@ -3,9 +3,14 @@ module Path.Meta where
 
 import Path.Name
 
-data Module v f = Module
+data Module v t f = Module
   { moduleName    :: ModuleName
   , moduleImports :: [f Import]
+  , moduleDecls   :: [f (Decl v t)]
   }
 
 data Import (f :: (* -> *) -> * -> *) = Import { importModuleName :: ModuleName }
+
+data Decl v t (f :: (* -> *) -> * -> *)
+  = Decl v t
+  | Defn v t
