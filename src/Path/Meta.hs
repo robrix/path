a2be97295f47535c668dfb91b9ab02ff063db50b
Rob Rix <rob.rix@me.com>
2018-12-20 13:05:39 -0500

    Stub in syntax for modules.

--- src/Path/Meta.hs
+++ src/Path/Meta.hs
@@ -3,4 +3,9 @@ module Path.Meta where
 
 import Path.Name
 
+data Module v f = Module
+  { moduleName    :: ModuleName
+  , moduleImports :: [f Import]
+  }
+
 data Import (f :: (* -> *) -> * -> *) = Import { importModuleName :: ModuleName }
