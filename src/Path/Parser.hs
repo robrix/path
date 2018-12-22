a7e4b3a31eea2772b00485401d1df3c27ca0d1bf
Rob Rix <rob.rix@me.com>
2018-12-21 21:56:09 -0500

    Define a helper to parse semicolon-separated blocks.

--- src/Path/Parser.hs
+++ src/Path/Parser.hs
@@ -10,6 +10,7 @@ module Path.Parser
 , reservedWords
 , reservedOperators
 , op
+, layout
 , ErrInfo
 , Span(..)
 , Delta(..)
@@ -88,3 +89,7 @@ keyword, op :: TokenParsing m => String -> m String
 keyword s = token (highlight ReservedIdentifier (try (string s <* notFollowedBy alphaNum))) <?> s
 
 op s = token (highlight Operator (string s)) <?> s
+
+
+layout :: TokenParsing m => m a -> m [a]
+layout m = braces (semiSep m) <|> semiSep m
