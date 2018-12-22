36a33be820374bf6746ca37d79c9bbf471e237b0
Rob Rix <rob.rix@me.com>
2018-12-21 23:15:04 -0500

    Revert "Parse imports and decls as a layout block."
    This reverts commit cfb3b746c702de1d42b9e9cd8b4dc75776491179.

--- src/Path/Parser/Module.hs
+++ src/Path/Parser/Module.hs
@@ -1,7 +1,6 @@
 module Path.Parser.Module where
 
 import Control.Applicative ((<**>), Alternative(..))
-import Data.Either (partitionEithers)
 import qualified Path.Module as Module
 import Path.Name
 import Path.Parser
@@ -11,8 +10,8 @@ import Path.Term
 import Text.Trifecta
 
 module' :: (DeltaParsing m, MonadFresh m) => FilePath -> m (Module.Module Name (Term (Surface Name) Span) Span)
-module' path = make <$> optional docs <* keyword "module" <*> moduleName <*> layout (Left <$> import' <|> Right <$> declaration)
-  where make comment name contents = let (imports, decls) = partitionEithers contents in Module.Module name comment path imports decls
+module' path = make <$> optional docs <* keyword "module" <*> moduleName <*> many import' <*> many declaration
+  where make comment name = Module.Module name comment path
 
 moduleName :: (Monad m, TokenParsing m) => m ModuleName
 moduleName = makeModuleName <$> token (runUnspaced (identifier `sepByNonEmpty` dot))
