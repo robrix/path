5c6fc236760c752d8be9e63cc76d083ea743b1ba
Rob Rix <rob.rix@me.com>
2018-12-22 11:24:22 -0500

    Define a datatype of operators.

--- src/Path/Parser/Mixfix.hs
+++ src/Path/Parser/Mixfix.hs
@@ -1,1 +1,9 @@
 module Path.Parser.Mixfix where
+
+import Data.List.NonEmpty (NonEmpty(..))
+
+data Operator
+  = Prefix (NonEmpty String)
+  | Postfix (NonEmpty String)
+  | Infix (NonEmpty String)
+  | Closed String (NonEmpty String)
