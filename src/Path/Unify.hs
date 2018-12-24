5b957a8b9239c000eff4cb238d17869f88bf85b6
Rob Rix <rob.rix@me.com>
2018-12-24 16:32:38 -0500

    Define a bunch of metatheoretical stuff like in the paper.

--- src/Path/Unify.hs
+++ src/Path/Unify.hs
@@ -5,13 +5,8 @@ import Control.Effect
 import Control.Effect.Fresh
 import Control.Effect.Reader
 import Control.Effect.State
-import qualified Path.Context as Context
-import Path.Core
-import Path.Name
-import Path.Subst
-import qualified Path.Term as Term
-import Path.Usage
-import Path.Value
+import Control.Monad.Fail
+import Path.Name hiding (Assoc(..))
 
 data Back a = Nil | Back a :> a
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
@@ -25,14 +20,83 @@ instance Monoid (Back a) where
   mempty = Nil
   mappend = (<>)
 
+data Term
+  = N Head (Back Elim)
+  | L (Bind QName Term)
+  | Set
+  | Pi Type (Bind QName Type)
+  deriving (Eq, Ord, Show)
 
-type Term = Term.Term (Core QName) ()
-type Type = Context.Type QName
+newtype Elim = A Term
+  deriving (Eq, Ord, Show)
+
+type Type = Term
 
 data Twin = Only | TwinL | TwinR
   deriving (Eq, Ord, Show)
 
 data Head = V QName Twin | M QName
+  deriving (Eq, Ord, Show)
+
+type Bind = (,)
+
+type Subs = [(QName, Term)]
+
+bind :: a -> b -> Bind a b
+bind = (,)
+
+unbind :: Applicative m => Bind a b -> m (a, b)
+unbind = pure
+
+h :: Head -> Term
+h h = N h Nil
+
+twin :: QName -> Twin -> Term
+twin q w = h (V q w)
+
+twinL :: QName -> Term
+twinL q = twin q TwinL
+
+twinR :: QName -> Term
+twinR q = twin q TwinL
+
+var :: QName -> Term
+var q = twin q Only
+
+meta :: QName -> Term
+meta q = h (M q)
+
+($$) :: Term -> Term -> Term
+f $$ a = f %% A a
+
+($*$) :: Term -> Back (QName, Type) -> Term
+f $*$ _Gam = foldl ($$) f (fmap (var . fst) _Gam)
+
+(%%) :: Term -> Elim -> Term
+L b   %% A a = eval [(x, a)] t where (x, t) = b
+N h e %% z   = N h (e :> z)
+_     %% _   = error "bad elimination"
+
+eval :: Subs -> Term -> Term
+eval g (L b)        = L (evalUnder g b)
+eval g (N h e)      = foldl (%%) (evalHead g h) (fmap (mapElim (eval g)) e)
+eval _ Set          = Set
+eval g (Pi _S _T)   = Pi (eval g _S) (evalUnder g _T)
+
+evalHead :: Subs -> Head -> Term
+evalHead g (V x _)    | Just t <- lookup x g      = t
+evalHead g (M alpha)  | Just t <- lookup alpha g  = t
+evalHead _ h                                      = N h Nil
+
+evalUnder :: Subs -> Bind QName Term -> Bind QName Term
+evalUnder g b = bind x (eval g t)
+                  where (x, t) = b
+
+mapElim :: (Term -> Term) -> Elim -> Elim
+mapElim f (A a) = A (f a)
+
+foldMapElim :: (Term -> m) -> Elim -> m
+foldMapElim f (A a) = f a
 
 data Equation = (Term, Type) :==: (Term, Type)
   deriving (Eq, Ord, Show)
@@ -62,15 +126,24 @@ data Problem
   deriving (Eq, Ord, Show)
 
 
-type Telescope = Back (QName, Usage, Type)
+type Telescope = Back (QName, Type)
 
-telescope :: Type -> (Telescope, Type)
-telescope (VPi n u t b) = let (tel, b') = telescope (b (vfree n)) in ((Nil :> (n, u, t)) <> tel, b')
-telescope t             = (Nil, t)
+telescope :: MonadFail m => Type -> m (Telescope, Type)
+telescope (Pi s t) = do
+  (x, t') <- unbind t
+  (tel, u) <- telescope t'
+  pure ((Nil :> (x, s)) <> tel, u)
+telescope t        = pure (Nil, t)
+
+_Pi :: QName -> Type -> Type -> Type
+_Pi x _S _T = Pi _S (bind x _T)
+
+_Pis :: Foldable f => f (QName, Type) -> Type -> Type
+_Pis xs _T = foldr (uncurry _Pi) _T xs
 
 
 type ContextL = Back Entry
-type ContextR = [Either (Subst QName Term) Entry]
+type ContextR = [Either Subs Entry]
 
 popL :: (Carrier sig m, Member (State ContextL) sig, Monad m) => m Entry
 popL = do
@@ -83,14 +156,14 @@ pushL :: (Carrier sig m, Member (State ContextL) sig, Monad m) => Entry -> m ()
 pushL e = modify (:> e)
 
 
-popR :: (Carrier sig m, Member (State ContextR) sig, Monad m) => m (Maybe (Either (Subst QName Term) Entry))
+popR :: (Carrier sig m, Member (State ContextR) sig, Monad m) => m (Maybe (Either Subs Entry))
 popR = do
   entries <- get
   case entries of
     e : es -> Just e <$ put es
     []     -> pure Nothing
 
-pushR :: (Carrier sig m, Member (State ContextR) sig, Monad m) => Either (Subst QName Term) Entry -> m ()
+pushR :: (Carrier sig m, Member (State ContextR) sig, Monad m) => Either Subs Entry -> m ()
 pushR e = modify (e:)
 
 lookupMeta :: (Carrier sig m, Member (State ContextL) sig, Monad m) => QName -> m Type
@@ -132,3 +205,11 @@ postpone s p = ask >>= pushR . Right . Q s . wrapProb p
 active, block :: (Carrier sig m, Member (Reader Params) sig, Member (State ContextR) sig, Monad m) => Problem -> m ()
 active = postpone Active
 block  = postpone Blocked
+
+
+hole :: (Carrier sig m, Member Fresh sig, Member (Reader ModuleName) sig, Member (State ContextL) sig, Member (State ContextR) sig, Monad m) => Telescope -> Type -> (Term -> m a) -> m a
+hole gam t f = do
+  alpha <- freshName
+  pushL $ E alpha (_Pis gam t) Hole
+  r <- f (meta alpha $*$ gam)
+  r <$ goLeft
