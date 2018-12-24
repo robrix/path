38b68a1890bf3f3fbde6b7e7914bea1703459a90
Rob Rix <rob.rix@me.com>
2018-12-23 22:54:20 -0500

    Define a bunch of other unification primitives.

--- src/Path/Unify.hs
+++ src/Path/Unify.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DeriveTraversable #-}
 module Path.Unify where
 
+import Path.Name
+
 data Back a = Nil | Back a :> a
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
@@ -12,3 +14,22 @@ data Equation tm ty = (tm, ty) :==: (tm, ty)
 
 sym :: Equation tm ty -> Equation tm ty
 sym (s :==: t) = t :==: s
+
+data Param ty = P ty | ty :++: ty
+  deriving (Eq, Ord, Show)
+
+data Dec a = Hole | Defn a
+  deriving (Eq, Ord, Show)
+
+data Entry tm ty
+  = E QName ty (Dec tm)
+  | Q Status (Problem tm ty)
+  deriving (Eq, Ord, Show)
+
+data Status = Blocked | Active
+  deriving (Eq, Ord, Show)
+
+data Problem tm ty
+  = Unify (Equation tm ty)
+  | All (Param ty) QName (Problem tm ty)
+  deriving (Eq, Ord, Show)
