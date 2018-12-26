2fb8f1da1659bb6b85c0cece9275a11f7fdd9b4f
Rob Rix <rob.rix@me.com>
2018-12-26 18:38:29 -0500

    Unify w.r.t. a type.

--- src/Path/Unify.hs
+++ src/Path/Unify.hs
@@ -6,7 +6,7 @@ import Control.Effect.Error
 import Control.Effect.Fresh
 import Control.Effect.Reader hiding (Reader(Local))
 import Control.Monad (unless)
-import Path.Context
+import Path.Context as Context
 import Path.Env as Env
 import Path.Error
 import Path.Eval
@@ -14,14 +14,15 @@ import Path.Name
 import Path.Value
 import Text.Trifecta.Rendering (Span)
 
-unify :: (Carrier sig m, Member (Error ElabError) sig, Member Fresh sig, Member (Reader Env) sig, Monad m) => Span -> Type QName -> Type QName -> m (Type QName)
+unify :: (Carrier sig m, Member (Error ElabError) sig, Member Fresh sig, Member (Reader Context) sig, Member (Reader Env) sig, Monad m) => Span -> Type QName -> Type QName -> Type QName -> m (Type QName)
 unify span = go
-  where go t1 t2 = case (t1, t2) of
-          (Type, Type) -> pure Type
-          (Lam n1 b1, Lam n2 b2) -> do
+  where go ty t1 t2 = case (ty, t1, t2) of
+          (Type, Type, Type) -> pure Type
+          (Pi tn u t b, Lam n1 b1, Lam n2 b2) -> do
             n <- freshName
-            local (Env.insert (Local n1) (vfree n) . Env.insert (Local n2) (vfree n)) (go b1 b2)
-          (t1, t2) -> do
+            local (Env.insert (Local n1) (vfree n) . Env.insert (Local n2) (vfree n))
+              (local (Context.insert (Local tn) t) (go b b1 b2))
+          (_, t1, t2) -> do
             t1' <- vforce t1
             unless (t1' `aeq` t2) (throwError (TypeMismatch t1 t2 span))
             pure t1'
