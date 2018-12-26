26256ebd0a37b016982974b720629d30e56e60ac
Rob Rix <rob.rix@me.com>
2018-12-26 17:32:17 -0500

    Force the actual type before unification.

--- src/Path/Unify.hs
+++ src/Path/Unify.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Path.Unify where
 
+import Control.Effect
 import Path.Context
+import Path.Env
+import Path.Eval
 import Path.Name
 import Path.Value
 
-unifiesWith :: Applicative m => Type QName -> Type QName -> m Bool
-unifiesWith t1 t2 = pure (t1 `aeq` t2)
+unifiesWith :: (Carrier sig m, Member (Reader Env) sig, Monad m) => Type QName -> Type QName -> m Bool
+unifiesWith t1 t2 = vforce t1 >>= \ t1 -> pure (t1 `aeq` t2)
