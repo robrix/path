bb4e6d59c7f5e954e05ec5211dff02d54726c10c
Rob Rix <rob.rix@me.com>
2018-12-26 18:30:17 -0500

    Close over the span.

--- src/Path/Unify.hs
+++ src/Path/Unify.hs
@@ -15,14 +15,15 @@ import Path.Value
 import Text.Trifecta.Rendering (Span)
 
 unifyWith :: (Carrier sig m, Member (Error ElabError) sig, Member Fresh sig, Member (Reader Env) sig, Monad m) => Span -> Type QName -> Type QName -> m ()
-unifyWith span t1 t2 = case (t1, t2) of
-  (Type, Type) -> pure ()
-  (Lam n1 b1, Lam n2 b2) -> do
-    n <- freshName
-    local (Env.insert (Local n1) (vfree n) . Env.insert (Local n2) (vfree n)) (unifyWith span b1 b2)
-  (t1, t2) -> do
-    t1' <- vforce t1
-    unless (t1' `aeq` t2) (throwError (TypeMismatch t1 t2 span))
+unifyWith span = go
+  where go t1 t2 = case (t1, t2) of
+          (Type, Type) -> pure ()
+          (Lam n1 b1, Lam n2 b2) -> do
+            n <- freshName
+            local (Env.insert (Local n1) (vfree n) . Env.insert (Local n2) (vfree n)) (go b1 b2)
+          (t1, t2) -> do
+            t1' <- vforce t1
+            unless (t1' `aeq` t2) (throwError (TypeMismatch t1 t2 span))
 
 
 freshName :: (Carrier sig m, Functor m, Member Fresh sig) => m QName
