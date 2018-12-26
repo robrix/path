5f3de4bf4b230dcc573cb43fefc1d58cb587671c
Rob Rix <rob.rix@me.com>
2018-12-26 18:07:51 -0500

    Define a helper to construct fresh names.

--- src/Path/Unify.hs
+++ src/Path/Unify.hs
@@ -2,6 +2,7 @@
 module Path.Unify where
 
 import Control.Effect
+import Control.Effect.Fresh
 import Path.Context
 import Path.Env
 import Path.Eval
@@ -10,3 +11,6 @@ import Path.Value
 
 unifiesWith :: (Carrier sig m, Member (Reader Env) sig, Monad m) => Type QName -> Type QName -> m Bool
 unifiesWith t1 t2 = vforce t1 >>= \ t1 -> pure (t1 `aeq` t2)
+
+freshName :: (Carrier sig m, Functor m, Member Fresh sig) => m QName
+freshName = Local . Gensym <$> fresh
