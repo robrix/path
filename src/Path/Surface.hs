8dba997220a7671af441fc4ab10fc589d1892300
Rob Rix <rob.rix@me.com>
2018-12-26 21:19:23 -0500

    :fire: the redundant Substitute instance for Surface.

--- src/Path/Surface.hs
+++ src/Path/Surface.hs
@@ -2,7 +2,6 @@
 module Path.Surface where
 
 import Path.Name
-import Path.Subst
 import Path.Term
 import Path.Usage
 
@@ -34,28 +33,6 @@ lam (n, a) b = In (Lam n b) (a <> ann b)
 
 (#) :: Semigroup ann => Term (Surface v) ann -> Term (Surface v) ann -> Term (Surface v) ann
 f # a = In (f :@ a) (ann f <> ann a)
-
-
-instance Substitute QName (Surface QName) where
-  subst i r = \case
-    In (ForAll v t t') ann
-      | i == Local v -> In (ForAll v (subst i r t) t') ann
-      | otherwise    -> In (ForAll v (subst i r t) (subst i r t')) ann
-    In (Var j) ann
-      | i == j    -> In r ann
-      | otherwise -> In (Var j) ann
-    In (Lam n b) ann
-      | i == Local n -> In (Lam n b) ann
-      | otherwise    -> In (Lam n (subst i r b)) ann
-    In (f :@ a) ann -> In (subst i r f :@ subst i r a) ann
-    In Type ann -> In Type ann
-    In (Pi n e t t') ann
-      | i == Local n -> In (Pi n e (subst i r t) t') ann
-      | otherwise    -> In (Pi n e (subst i r t) (subst i r t')) ann
-    In (Hole v) ann
-      | i == v    -> In r ann
-      | otherwise -> In (Hole v) ann
-    In Implicit ann -> In Implicit ann
 
 
 uses :: Name -> Term (Surface QName) a -> [a]
