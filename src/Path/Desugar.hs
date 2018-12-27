c0fee8572c0d4cead6f246a9aba3ffff08eec660
Rob Rix <rob.rix@me.com>
2018-12-26 21:17:05 -0500

    Define desugaring over sums of sugar, implicit, and core syntax.

--- src/Path/Desugar.hs
+++ src/Path/Desugar.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeOperators #-}
 module Path.Desugar where
 
 import Path.Core as Core
@@ -6,14 +7,8 @@ import Path.Surface as Surface
 import Path.Term
 import Path.Usage
 
-desugar :: Term (Surface QName) a
-        -> Maybe (Term (Core QName) a)
-desugar (In out span) = flip In span <$> case out of
-  Surface.Var n -> pure (Core.Var n)
-  Surface.Lam n b -> Core.Lam n <$> desugar b
-  f Surface.:@ a -> (Core.:@) <$> desugar f <*> desugar a
-  Surface.Type -> pure Core.Type
-  Surface.Pi n u t b -> Core.Pi n u <$> desugar t <*> desugar b
-  Surface.ForAll n t b -> Core.Pi n Zero <$> desugar t <*> desugar b
-  Surface.Hole _ -> Nothing
-  Surface.Implicit -> Nothing
+desugar :: Term (Sugar QName :+: Implicit QName :+: Core QName) a
+        -> Term (Implicit QName :+: Core QName) a
+desugar (In out span) = flip In span $ case out of
+  L (ForAll' n t b) -> R (Core.Pi n Zero (desugar t) (desugar b))
+  R rest -> desugar <$> rest
