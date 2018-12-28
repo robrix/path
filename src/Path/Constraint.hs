3bc5170fbdde6db374ec7b45fce822fac39a426f
Rob Rix <rob.rix@me.com>
2018-12-28 14:06:17 -0500

    Give a type of constraints.

--- src/Path/Constraint.hs
+++ src/Path/Constraint.hs
@@ -1,1 +1,15 @@
+{-# LANGUAGE TypeOperators #-}
 module Path.Constraint where
+
+import Path.Context
+import Path.Core
+import Path.Name
+import Path.Term
+import Text.Trifecta.Rendering (Span)
+
+data Constraint = Constraint
+  { constraintContext :: Context
+  , constraintLhs     :: Term (Implicit QName :+: Core Name QName) Span
+  , constraintRhs     :: Term (Implicit QName :+: Core Name QName) Span
+  , constraintType    :: Type QName
+  }
