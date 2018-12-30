498fcf78af57f926e26430aec8cd26b21ba488b4
Rob Rix <rob.rix@me.com>
2018-12-30 13:48:30 -0500

    Define instantiation constraints.

--- src/Path/Constraint.hs
+++ src/Path/Constraint.hs
@@ -1,16 +1,25 @@
 {-# LANGUAGE DeriveFunctor, ExistentialQuantification, GADTs, LambdaCase, StandaloneDeriving #-}
 module Path.Constraint where
 
+import Path.Context
+import Path.Name
+
 data Constraint a where
   Top :: Constraint ()
   (:/\:) :: Constraint a -> Constraint b -> Constraint (a, b)
   (:=:) :: Int -> Int -> Constraint ()
+  Instance :: QName -> Int -> Constraint [Type QName]
 
 data Witness a where
   WUnit :: Witness ()
   WConj :: Witness a -> Witness b -> Witness (a, b)
+  WInst :: [Type QName] -> Witness [Type QName]
 
 data Solver m a = forall b . Solver (Constraint b) (Witness b -> m a)
+
+instance' :: Applicative m => QName -> Int -> Solver m [Type QName]
+instance' x v = Solver (Instance x v) (\case
+  WInst tys -> pure tys)
 
 deriving instance Functor m => Functor (Solver m)
 
