b32b9d6e90bd955b61138664348545c454bbfc33
Rob Rix <rob.rix@me.com>
2018-12-30 14:38:01 -0500

    Run solvers in the monad.

--- src/Path/Constraint.hs
+++ src/Path/Constraint.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveFunctor, ExistentialQuantification, GADTs, LambdaCase, StandaloneDeriving #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, FlexibleContexts, GADTs, LambdaCase, StandaloneDeriving #-}
 module Path.Constraint where
 
 import Path.Context
@@ -15,19 +15,21 @@ data Witness a where
   WConj :: Witness a -> Witness b -> Witness (a, b)
   WInst :: [Type QName] -> Witness [Type QName]
 
-data Solver m a = forall b . Solver (Constraint b) (Witness b -> m a)
+data Solver m a = forall b . Solver (m (Constraint b, Witness b -> m a))
 
 (~~) :: Applicative m => Int -> Int -> Solver m ()
-v1 ~~ v2 = Solver (v1 :==: v2) (const (pure ()))
+v1 ~~ v2 = Solver (pure (v1 :==: v2, const (pure ())))
 
 instance' :: Applicative m => QName -> Int -> Solver m [Type QName]
-instance' x v = Solver (Instance x v) (\case
-  WInst tys -> pure tys)
+instance' x v = Solver (pure (Instance x v, \case
+  WInst tys -> pure tys))
 
 deriving instance Functor m => Functor (Solver m)
 
 instance Applicative m => Applicative (Solver m) where
-  pure a = Solver Top (const (pure a))
+  pure a = Solver (pure (Top, const (pure a)))
 
-  Solver c1 f <*> Solver c2 a = Solver (c1 :/\: c2) (\case
-    WConj wf wa -> f wf <*> a wa)
+  Solver f <*> Solver a = Solver (combine <$> f <*> a)
+    where combine :: Applicative m => (Constraint x, Witness x -> m (a -> b)) -> (Constraint y, Witness y -> m a) -> (Constraint (x, y), Witness (x, y) -> m b)
+          combine (c1, f') (c2, a') = (c1 :/\: c2, \case
+            WConj wf wa -> f' wf <*> a' wa)
