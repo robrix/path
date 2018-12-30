dfd6038afcd545220940cb179495262f977d9567
Rob Rix <rob.rix@me.com>
2018-12-30 14:49:52 -0500

    Define an HFunctor instance for Solver.

--- src/Path/Constraint.hs
+++ src/Path/Constraint.hs
@@ -1,9 +1,15 @@
 {-# LANGUAGE DeriveFunctor, KindSignatures #-}
 module Path.Constraint where
 
+import Control.Effect.Carrier
+import Data.Coerce
+
 newtype MetaVar = M Int
   deriving (Eq, Ord, Show)
 
 data Solver (m :: * -> *) a
   = Unify MetaVar MetaVar a
   deriving (Functor)
+
+instance HFunctor Solver where
+  hmap _ = coerce
