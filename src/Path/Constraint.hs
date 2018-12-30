4c735316439efbc4ebdbf25e524a096cd57ce42d
Rob Rix <rob.rix@me.com>
2018-12-30 13:11:56 -0500

    Constraints and witnesses are typed.

--- src/Path/Constraint.hs
+++ src/Path/Constraint.hs
@@ -1,25 +1,21 @@
-{-# LANGUAGE DeriveFunctor, TypeOperators #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, GADTs, LambdaCase, StandaloneDeriving #-}
 module Path.Constraint where
 
-import Path.Context
-import Path.Name
+data Constraint a where
+  Top :: Constraint ()
+  (:/\:) :: Constraint a -> Constraint b -> Constraint (a, b)
+  (:=:) :: Int -> Int -> Constraint ()
 
-data Constraint
-  = Top
-  | Constraint :/\: Constraint
-  | Int :=: Int
-  | Exists Int Constraint
-  | Instance Name Int
-  | Def Name Int Constraint
-  | Let Name Int Constraint Constraint
-  | Int :@ (Type QName)
+data Witness a where
+  WUnit :: Witness ()
+  WConj :: Witness a -> Witness b -> Witness (a, b)
 
-data Witness = Witness
+data Solver m a = forall b . Solver (Constraint b) (Witness b -> m a)
 
-data Solver m a = Solver Constraint (Witness -> m a)
-  deriving (Functor)
+deriving instance Functor m => Functor (Solver m)
 
 instance Applicative m => Applicative (Solver m) where
   pure a = Solver Top (const (pure a))
 
-  Solver c1 f <*> Solver c2 a = Solver (c1 :/\: c2) ((<*>) <$> f <*> a)
+  Solver c1 f <*> Solver c2 a = Solver (c1 :/\: c2) (\case
+    WConj wf wa -> f wf <*> a wa)
