4800e162ab47ae5a8d7eace1bbce961828cac6e5
Rob Rix <rob.rix@me.com>
2018-12-31 17:32:38 -0500

    Define a function for running constraints.

--- src/Path/Unify.hs
+++ src/Path/Unify.hs
@@ -1,5 +1,7 @@
 module Path.Unify where
 
+import Control.Effect
+import Control.Effect.State
 import Path.Core
 import Path.Name
 import Path.Term
@@ -10,3 +12,6 @@ data Constraint
   | Constraint :/\: Constraint
   | Meta :==: Term (Core Name QName) Span
   deriving (Eq, Ord, Show)
+
+runConstraints :: (Carrier sig m, Effect sig, Functor m) => Eff (StateC Constraint m) a -> m a
+runConstraints = evalState Top
