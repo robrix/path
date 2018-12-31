5d5a9df047084b1f9cc4f55c78437d9f49aca6c6
Rob Rix <rob.rix@me.com>
2018-12-31 17:20:13 -0500

    Define equality constraints.

--- src/Path/Unify.hs
+++ src/Path/Unify.hs
@@ -1,6 +1,12 @@
 module Path.Unify where
 
+import Path.Core
+import Path.Name
+import Path.Term
+import Text.Trifecta.Rendering (Span)
+
 data Constraint
   = Top
   | Constraint :/\: Constraint
+  | Meta :==: Term (Core Name QName) Span
   deriving (Eq, Ord, Show)
