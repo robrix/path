425422eadd4afd42fe981c316fbc6227d95861e0
Rob Rix <rob.rix@me.com>
2019-01-02 15:00:00 -0500

    Define a Pretty instance for Entry.

--- src/Path/Scope.hs
+++ src/Path/Scope.hs
@@ -1,8 +1,15 @@
 module Path.Scope where
 
 import Path.Context
+import Path.Pretty
 import Path.Value
 
 data Entry
   = Decl Type
   | Defn (Typed Value)
+
+instance Pretty Entry where
+  pretty (Decl        ty)  =                             colon <+> pretty ty
+  pretty (Defn (v ::: ty)) = pretty "=" <+> pretty v <+> colon <+> pretty ty
+
+instance PrettyPrec Entry
