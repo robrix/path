d05801a458516c89e256502c56bdb52167a36fa6
Rob Rix <rob.rix@me.com>
2019-01-02 16:28:47 -0500

    Envs define only local names.

--- src/Path/Env.hs
+++ src/Path/Env.hs
@@ -7,22 +7,22 @@ import Path.Name
 import Path.Pretty
 import Path.Value
 
-newtype Env = Env { unEnv :: Map.Map QName Value }
+newtype Env = Env { unEnv :: Map.Map Name Value }
   deriving (Eq, Monoid, Ord, Semigroup, Show)
 
 null :: Env -> Bool
 null = Map.null . unEnv
 
-lookup :: QName -> Env -> Maybe Value
+lookup :: Name -> Env -> Maybe Value
 lookup n = Map.lookup n . unEnv
 
-insert :: QName -> Value -> Env -> Env
+insert :: Name -> Value -> Env -> Env
 insert n v = Env . Map.insert n v . unEnv
 
 union :: Env -> Env -> Env
 union (Env e1) (Env e2) = Env (Map.union e1 e2)
 
-filter :: (QName -> Value -> Bool) -> Env -> Env
+filter :: (Name -> Value -> Bool) -> Env -> Env
 filter f = Env . Map.filterWithKey f . unEnv
 
 instance Pretty Env where
--- src/Path/Eval.hs
+++ src/Path/Eval.hs
@@ -15,18 +15,17 @@ import Path.Value as Value
 eval :: (Carrier sig m, Functor m, Member (Reader Env) sig) => Term (Core Name QName) a -> m Value
 eval tm = asks (flip go tm)
   where go env = \case
-          In (Core.Var n) _
-            | isLocal n -> fromMaybe (vfree n) (Env.lookup n env)
-            | otherwise -> vfree n
-          In (Core.Lam n b) _ -> Value.Lam (\ v -> go (Env.insert (Local n) v env) b)
+          In (Core.Var (Local n)) _ -> fromMaybe (vfree (Local n)) (Env.lookup n env)
+          In (Core.Var (m :.: n)) _ -> vfree (m :.: n)
+          In (Core.Lam n b) _ -> Value.Lam (\ v -> go (Env.insert n v env) b)
           In (f :$ a) _ -> go env f $$ go env a
           In Core.Type _ -> Value.Type
-          In (Core.Pi n p u t b) _ -> Value.Pi p u (go env t) (\ v -> go (Env.insert (Local n) v env) b)
+          In (Core.Pi n p u t b) _ -> Value.Pi p u (go env t) (\ v -> go (Env.insert n v env) b)
 
 -- | Evaluate a 'Value' to weak head normal form.
 --
 --   This involves looking up variables at the head of neutral terms in the environment, but will leave other values alone, as they’re already constructor-headed.
 whnf :: (Carrier sig m, Member (Reader Env) sig, Member (Reader Scope) sig, Monad m) => Value -> m Value
 whnf (sp :& (m :.: n)) = asks (entryValue <=< Scope.lookup (m :.: n)) >>= maybe (pure (sp :& (m :.: n))) (whnf . ($$* sp))
-whnf (sp :& n)         = asks (Env.lookup n) >>= maybe (pure (sp :& n)) (whnf . ($$* sp))
+whnf (sp :& (Local n)) = asks (Env.lookup n) >>= maybe (pure (sp :& Local n)) (whnf . ($$* sp))
 whnf v                 = pure v
