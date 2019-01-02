9fa7c3fbc6428b23a8a133555faab1d5011b76a6
Rob Rix <rob.rix@me.com>
2019-01-01 21:50:13 -0500

    Test that applications are quoted in correct sequence.

--- src/Path/Value.hs
+++ src/Path/Value.hs
@@ -50,6 +50,7 @@ vapp f a = error ("illegal application of " <> show f <> " to " <> show a)
 --   prop> quote i Type == In Core.Type ()
 --   prop> quote i (Lam id) == In (Core.Lam (Gensym "" i) (In (Core.Var i) ())) ()
 --   prop> quote i (Pi Im Zero Type id) == In (Core.Pi Im Zero Type (Gensym "" i) (In (Core.Var i) ())) ()
+  -- prop> quote i ((vfree (Local (Name s)) `vapp` vfree (Local (Name t))) `vapp` vfree (Local (Name u))) == In (In (In (Core.Var (Local (Name s))) () Core.:$ In (Core.Var (Local (Name t))) ()) () :$ In (Core.Var (Local (Name u))) ()) ()
 quote :: Int -> Value -> Term (Core.Core Name QName) ()
 quote i = \case
   Type -> In Core.Type ()
