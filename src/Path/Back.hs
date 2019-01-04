c09ecb4fb4f2372abfe34454a45c3b97aad7bf47
Rob Rix <rob.rix@me.com>
2019-01-03 22:56:05 -0500

    Define a Pretty instance for Back.

--- src/Path/Back.hs
+++ src/Path/Back.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE DeriveTraversable, LambdaCase #-}
 module Path.Back where
 
-import Data.Foldable (foldl')
+import Data.Foldable (foldl', toList)
+import Path.Pretty
 import Prelude hiding (filter, lookup)
 
 data Back a = Nil | Back a :> a
@@ -17,6 +18,11 @@ instance Semigroup (Back a) where
 instance Monoid (Back a) where
   mempty = Nil
   mappend = (<>)
+
+instance Pretty a => Pretty (Back a) where
+  pretty = list . toList . fmap pretty
+
+instance Pretty a => PrettyPrec (Back a)
 
 
 fromList :: [a] -> Back a
