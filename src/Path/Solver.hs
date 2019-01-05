459d42af63b9e4958db0560fd623c90d5c19a529
Rob Rix <rob.rix@me.com>
2019-01-05 18:26:27 -0500

    :fire: redundant parens.

--- src/Path/Solver.hs
+++ src/Path/Solver.hs
@@ -10,5 +10,5 @@ import Text.Trifecta.Rendering (Span)
 
 simplify :: (Carrier sig m, Member (Error ElabError) sig, Monad m) => Span -> Equation -> m (Set.Set Equation)
 simplify span = \case
-  (tm1 ::: ty1 :===: tm2 ::: ty2) | ty1 == ty2, tm1 == tm2 -> pure Set.empty
+  tm1 ::: ty1 :===: tm2 ::: ty2 | ty1 == ty2, tm1 == tm2 -> pure Set.empty
   q -> throwError (ElabError span mempty (TypeMismatch q))
