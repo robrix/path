139835dc616a2f94997753771934e1d6b531f371
Rob Rix <rob.rix@me.com>
2019-01-06 18:38:00 -0500

    Rename the span parameter.

--- src/Path/Error.hs
+++ src/Path/Error.hs
@@ -27,20 +27,20 @@ data ErrorReason
   deriving (Eq, Ord, Show)
 
 instance Pretty ElabError where
-  pretty (ElabError spans ctx reason) = case reason of
-    FreeVariable name -> prettyErr spans (pretty "free variable" <+> squotes (pretty name)) (prettyCtx ctx)
-    TypeMismatch (expected :===: actual :@ _) -> prettyErr spans (fold (punctuate hardline
+  pretty (ElabError span ctx reason) = case reason of
+    FreeVariable name -> prettyErr span (pretty "free variable" <+> squotes (pretty name)) (prettyCtx ctx)
+    TypeMismatch (expected :===: actual :@ _) -> prettyErr span (fold (punctuate hardline
       [ pretty "type mismatch"
       , pretty "expected:" <+> pretty expected
       , pretty "  actual:" <+> pretty actual
       ])) (prettyCtx ctx)
-    NoRuleToInfer -> prettyErr spans (pretty "no rule to infer type of term") (prettyCtx ctx)
-    IllegalApplication ty -> prettyErr spans (pretty "illegal application of term of type" <+> pretty ty) (prettyCtx ctx)
-    ResourceMismatch n pi used uses -> prettyErr spans msg (prettyCtx ctx <> map prettys uses)
+    NoRuleToInfer -> prettyErr span (pretty "no rule to infer type of term") (prettyCtx ctx)
+    IllegalApplication ty -> prettyErr span (pretty "illegal application of term of type" <+> pretty ty) (prettyCtx ctx)
+    ResourceMismatch n pi used uses -> prettyErr span msg (prettyCtx ctx <> map prettys uses)
       where msg = pretty "Variable" <+> squotes (pretty n) <+> pretty "used" <+> pretty (if pi > used then "less" else "more") <+> parens (pretty (length uses)) <+> pretty "than required" <+> parens (pretty pi)
-    TypedHole n ty -> prettyErr spans msg (prettyCtx ctx)
+    TypedHole n ty -> prettyErr span msg (prettyCtx ctx)
       where msg = pretty "Found hole" <+> squotes (pretty n) <+> pretty "of type" <+> squotes (pretty ty)
-    InfiniteType n t -> prettyErr spans (pretty "Cannot construct infinite type" <+> pretty n <+> blue (pretty "~") <+> pretty t) (prettyCtx ctx)
+    InfiniteType n t -> prettyErr span (pretty "Cannot construct infinite type" <+> pretty n <+> blue (pretty "~") <+> pretty t) (prettyCtx ctx)
     where prettyCtx ctx
             =  if (Context.null ctx) then [] else [nest 2 (vsep [pretty "Local bindings:", pretty ctx])]
 
