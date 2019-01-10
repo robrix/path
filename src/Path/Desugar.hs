49fdcf01858ff7296ffe1a717ba4e0162014c33f
Rob Rix <rob.rix@me.com>
2019-01-09 21:57:58 -0500

    Reformat desugar.

--- src/Path/Desugar.hs
+++ src/Path/Desugar.hs
@@ -9,12 +9,11 @@ import Path.Term
 
 desugar :: Term (Surface.Surface (Maybe UName) UName) a
         -> Term (Core (Maybe UName) UName) a
-desugar = cata go
-  where go out span = flip In span $ case out of
-          Surface.Free v -> Core.Free v
-          Surface.Lam n b -> Core.Lam n (Scope b)
-          f Surface.:$ a -> f Core.:$ a
-          Surface.Type -> Core.Type
-          Surface.Pi n p u t b -> Core.Pi n p u t (Scope b)
-          Surface.Hole v -> Core.Hole v
-          (u, a) Surface.:-> b -> Core.Pi Nothing Ex u a (Scope b)
+desugar = cata $ \ out span -> flip In span $ case out of
+  Surface.Free v -> Core.Free v
+  Surface.Lam n b -> Core.Lam n (Scope b)
+  f Surface.:$ a -> f Core.:$ a
+  Surface.Type -> Core.Type
+  Surface.Pi n p u t b -> Core.Pi n p u t (Scope b)
+  Surface.Hole v -> Core.Hole v
+  (u, a) Surface.:-> b -> Core.Pi Nothing Ex u a (Scope b)
