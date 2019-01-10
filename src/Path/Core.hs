523c23d056d7f9481550646b65570f0b38e62bd6
Rob Rix <rob.rix@me.com>
2019-01-09 19:49:37 -0500

    :fire: the PrettyPrec instance for Core.

--- src/Path/Core.hs
+++ src/Path/Core.hs
@@ -5,7 +5,6 @@ import qualified Data.Set as Set
 import Path.Implicit
 import Path.Name
 import Path.Plicity
-import Path.Pretty
 import Path.Term
 import Path.Usage
 
@@ -23,32 +22,6 @@ data (f :+: g) a
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 infixr 4 :+:
-
-instance PrettyPrec a => PrettyPrec (Core Name QName (Term (Core Name QName) a)) where
-  prettyPrec d = \case
-    Free n -> pretty n
-    Lam v b -> prettyParens (d > 0) $ align (group (cyan backslash <+> go v b))
-      where go v b = var v b <> case b of
-              In (Lam v' b') _ -> space <> go v' b'
-              _                -> line <> cyan dot <+> prettyPrec 0 b
-            var v b | Local v `Set.member` fvs b = pretty v
-                    | otherwise                  = pretty '_'
-    f :$ a -> prettyParens (d > 10) $ group (align (nest 2 (go f a)))
-      where go f a = case f of
-              In (f' :$ a') _ -> go f' a' </> prettyPrec 11 a
-              _               -> prettyPrec 10 f </> prettyPrec 11 a
-    Type -> yellow (pretty "Type")
-    Pi v ie pi t b
-      | Local v `Set.member` fvs b -> prettyParens (d > 1) $ withIe (pretty v <+> colon <+> withPi (prettyPrec 0 t)) <+> arrow <+> prettyPrec 1 b
-      | otherwise                  -> prettyParens (d > 1) $ withPi (prettyPrec 2 t <+> arrow <+> prettyPrec 1 b)
-      where withPi
-              | ie == Ex, pi == More = id
-              | ie == Im, pi == Zero = id
-              | otherwise  = (pretty pi <+>)
-            withIe
-              | ie == Im  = prettyBraces True
-              | otherwise = prettyParens True
-            arrow = blue (pretty "->")
 
 instance FreeVariables1 QName (Core Name QName) where
   liftFvs fvs = \case
