3f86dbc2d9995078614b799e0130e5e3b9453d66
Rob Rix <rob.rix@me.com>
2019-01-13 12:36:33 -0500

    Stub in a partial datatype.

--- src/Path/Partial.hs
+++ src/Path/Partial.hs
@@ -1,1 +1,16 @@
 module Path.Partial where
+
+import Path.Name
+import Path.Plicity
+import Path.Stack
+import Path.Usage
+
+data Partial
+  = Type
+  | Lam Scope
+  | Pi Plicity Usage Partial Scope
+  | Head :$ Stack Partial
+  deriving (Eq, Ord, Show)
+
+newtype Scope = Scope Partial
+  deriving (Eq, Ord, Show)
