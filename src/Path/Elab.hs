8f07830c72b6428b1a08be563b2b02495893f360
Rob Rix <rob.rix@me.com>
2019-01-18 18:04:45 -0500

    :fire: the Elab effect.

--- src/Path/Elab.hs
+++ src/Path/Elab.hs
@@ -2,15 +2,12 @@
 module Path.Elab where
 
 import Control.Effect
-import Control.Effect.Carrier
 import Control.Effect.Error
 import Control.Effect.Fresh
 import Control.Effect.Reader hiding (Reader(Local))
 import Control.Effect.State
-import Control.Effect.Sum
 import Control.Effect.Writer
 import Control.Monad ((<=<), unless, when)
-import Data.Coerce (coerce)
 import Data.Foldable (for_)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
@@ -33,18 +30,6 @@ import Path.Usage
 import Path.Value as Value hiding (Scope(..))
 import Text.Trifecta.Rendering (Span(..))
 
-data Elab (m :: * -> *) k
-  = Infer        Core.Core  (Typed Value -> k)
-  | Check (Typed Core.Core) (Typed Value -> k)
-  deriving (Functor)
-
-instance HFunctor Elab where
-  hmap _ = coerce
-
-instance Effect Elab where
-  handle state handler = coerce . fmap (handler . (<$ state))
-
-
 runElab :: ( Carrier sig m
            , Effect sig
            , Member (Error ElabError) sig
@@ -53,130 +38,111 @@ runElab :: ( Carrier sig m
            , Monad m
            )
         => Usage
-        -> Eff (ElabC m) (Typed Value)
+        -> Eff (ReaderC Span (Eff (ReaderC Usage (Eff (ReaderC Context (Eff (WriterC Resources (Eff (WriterC (Set.Set (Caused (Equation Value))) (Eff (FreshC m))))))))))) (Typed Value)
         -> m (Resources, Typed Value)
-runElab sigma = local (// "elab") . solveAndApply <=< runFresh . runWriter . runWriter . runReader mempty . runReader sigma . runReader (Span mempty mempty mempty) . runElabC . interpret
+runElab sigma = local (// "elab") . solveAndApply <=< runFresh . runWriter . runWriter . runReader mempty . runReader sigma . runReader (Span mempty mempty mempty)
   where solveAndApply (eqns, (res, tm ::: ty)) = do
           subst <- solve eqns
           pure (res, apply subst tm ::: apply subst ty)
 
-newtype ElabC m a = ElabC { runElabC :: Eff (ReaderC Span (Eff (ReaderC Usage (Eff (ReaderC Context (Eff (WriterC Resources (Eff (WriterC (Set.Set (Caused (Equation Value))) (Eff (FreshC m))))))))))) a }
-  deriving (Applicative, Functor, Monad)
-
-instance ( Carrier sig m
-         , Effect sig
-         , Member (Error ElabError) sig
-         , Member (Reader Gensym) sig
-         , Member (Reader Scope) sig
-         , Monad m
-         )
-      => Carrier (Elab :+: sig) (ElabC m) where
-  ret = ElabC . ret
-  eff = handleSum (ElabC . eff . R . R . R . R . R . R . handleCoercible) (\case
-    Infer tm k -> k =<< case tm of
-      Core.Type -> pure (Value.Type ::: Value.Type)
-      Core.Pi i e t b -> ElabC (gensym "") >>= \ n -> do
-        t' ::: _ <- check (t ::: Value.Type)
-        b' ::: _ <- n ::: t' |- check (Core.instantiate (Core.free (Local n)) b ::: Value.Type)
-        pure (Value.pi ((n, i, e) ::: t') b' ::: Value.Type)
-      Core.Head (Free n) -> do
-        t <- lookupVar n >>= whnf
-        sigma <- askSigma
-        ElabC (tell (Resources.singleton (Q n) sigma))
-        elabImplicits (free (Q n ::: t) ::: t)
-      f Core.:$ a -> do
-        f' ::: fTy <- infer f
-        (pi, t, b) <- whnf fTy >>= ensurePi
-        a' ::: _ <- raise (censor (Resources.mult pi)) (check (a ::: t))
-        pure (f' $$ a' ::: instantiate a' b)
-      Core.Lam b -> ElabC (gensym "") >>= \ n -> do
-        (_, t) <- exists Value.Type
-        e' ::: eTy <- n ::: t |- raise (censor (Resources.delete (qlocal n))) (infer (Core.instantiate (Core.free (Local n)) b))
-        pure (Value.lam (qlocal n ::: t) e' ::: Value.pi ((n, Ex, More) ::: t) eTy)
-      Core.Hole _ -> do
-        (_, ty) <- exists Value.Type
-        (_, m) <- exists ty
-        pure (m ::: ty)
-      Core.Ann ann t -> raise (local (const ann)) (infer t)
-      _ -> throwElabError NoRuleToInfer
-
-    Check (tm ::: ty) k -> k =<< case tm ::: ty of
-      _ ::: Value.Pi Im pi t b -> ElabC (gensym "") >>= \ n -> raise (censor (Resources.delete (qlocal n))) $ do
-        (res, e' ::: _) <- n ::: t |- raise listen (check (tm ::: instantiate (free (qlocal n ::: t)) b))
-        verifyResources tm n pi res
-        pure (Value.lam (qlocal n ::: t) e' ::: ty)
-      Core.Lam e ::: Value.Pi Ex pi t b -> ElabC (gensym "") >>= \ n -> raise (censor (Resources.delete (qlocal n))) $ do
-        (res, e' ::: _) <- n ::: t |- raise listen (check (Core.instantiate (Core.free (Local n)) e ::: instantiate (free (qlocal n ::: t)) b))
-        verifyResources tm n pi res
-        pure (Value.lam (qlocal n ::: t) e' ::: ty)
-      Core.Hole _ ::: ty -> do
-        (_, m) <- exists ty
-        pure (m ::: ty)
-      Core.Ann ann tm ::: ty -> raise (local (const ann)) (check (tm ::: ty))
-      _ ::: ((Free (Q (_ :.: _)) ::: _) Value.:$ _) -> do
-       ty' <- whnf ty
-       check (tm ::: ty')
-      _ ::: ty -> do
-        tm' ::: inferred <- infer tm
-        unified <- unify Value.Type (ty :===: inferred)
-        pure (tm' ::: unified))
-    where n ::: t |- m = raise (local (Context.insert (n ::: t))) m
-          infix 5 |-
-          raise f (ElabC m) = ElabC (f m)
-
-          verifyResources tm n pi br = do
-            let used = Resources.lookup (qlocal n) br
-            sigma <- askSigma
-            unless (sigma >< pi == More) . when (pi /= used) $
-              throwElabError (ResourceMismatch n pi used (Core.uses n tm))
-
-          elabImplicits = \case
-            tm ::: Value.Pi Im _ t b -> do
-              (n, v) <- exists t
-              sigma <- askSigma
-              ElabC (tell (Resources.singleton n sigma))
-              elabImplicits (tm $$ v ::: instantiate v b)
-            tm -> pure tm
-
-          ensurePi t = case t of
-            Value.Pi _ pi t b -> pure (pi, t, b)
-            (Free (M _) ::: _) Value.:$ _ -> do
-              (mA, _A) <- exists Value.Type
-              (_, _B) <- exists _A
-              let _B' = bind mA _B
-              span <- ElabC ask
-              (More, _A, _B') <$ ElabC (tell (Set.singleton (t :===: Value.Pi Ex More _A _B' :@ Assert span)))
-            _ -> throwElabError (IllegalApplication t)
-
-          unify ty (tm1 :===: tm2) = if tm1 == tm2 then pure tm1 else do
-            (_, v) <- exists ty
-            span <- ElabC ask
-            v <$ ElabC (tell (Set.fromList [ v :===: tm1 :@ Assert span
-                                           , v :===: tm2 :@ Assert span ]))
-
-          throwElabError reason = ElabError <$> ElabC ask <*> askContext <*> pure reason >>= throwError
-
-          askContext = ElabC ask
-          askSigma = ElabC ask
-
-          exists t = do
-            Context c <- askContext
-            n <- M . Meta <$> ElabC (gensym "_meta_")
-            pure (n, free (n ::: lams (fmap (fmap qlocal) c) t) $$* fmap (free . fmap qlocal) c)
-
-          lookupVar (m :.: n) = asks (Scope.lookup (m :.: n)) >>= maybe (throwElabError (FreeVariable (m :.: n))) (pure . entryType)
-          lookupVar (Local n) = ElabC (asks (Context.lookup n)) >>= maybe (throwElabError (FreeVariable (Local n))) pure
-
-
-infer :: (Carrier sig m, Member Elab sig)
+infer :: (Carrier sig m, Member (Error ElabError) sig, Member Fresh sig, Member (Reader Context) sig, Member (Reader Gensym) sig, Member (Reader Scope) sig, Member (Reader Span) sig, Member (Reader Usage) sig, Member (Writer Resources) sig, Member (Writer (Set.Set (Caused (Equation Value)))) sig, Monad m)
       => Core.Core
       -> m (Typed Value)
-infer tm = send (Infer tm ret)
+infer = \case
+  Core.Type -> pure (Value.Type ::: Value.Type)
+  Core.Pi i e t b -> gensym "" >>= \ n -> do
+    t' ::: _ <- check (t ::: Value.Type)
+    b' ::: _ <- n ::: t' |- check (Core.instantiate (Core.free (Local n)) b ::: Value.Type)
+    pure (Value.pi ((n, i, e) ::: t') b' ::: Value.Type)
+  Core.Head (Free n) -> do
+    t <- lookupVar n >>= whnf
+    sigma <- ask
+    tell (Resources.singleton (Q n) sigma)
+    elabImplicits (free (Q n ::: t) ::: t)
+  f Core.:$ a -> do
+    f' ::: fTy <- infer f
+    (pi, t, b) <- whnf fTy >>= ensurePi
+    a' ::: _ <- censor (Resources.mult pi) (check (a ::: t))
+    pure (f' $$ a' ::: instantiate a' b)
+  Core.Lam b -> gensym "" >>= \ n -> do
+    (_, t) <- exists Value.Type
+    e' ::: eTy <- n ::: t |- censor (Resources.delete (qlocal n)) (infer (Core.instantiate (Core.free (Local n)) b))
+    pure (Value.lam (qlocal n ::: t) e' ::: Value.pi ((n, Ex, More) ::: t) eTy)
+  Core.Hole _ -> do
+    (_, ty) <- exists Value.Type
+    (_, m) <- exists ty
+    pure (m ::: ty)
+  Core.Ann ann t -> local (const ann) (infer t)
+  _ -> throwElabError NoRuleToInfer
+  where elabImplicits = \case
+          tm ::: Value.Pi Im _ t b -> do
+            (n, v) <- exists t
+            sigma <- ask
+            tell (Resources.singleton n sigma)
+            elabImplicits (tm $$ v ::: instantiate v b)
+          tm -> pure tm
 
-check :: (Carrier sig m, Member Elab sig)
+        ensurePi t = case t of
+          Value.Pi _ pi t b -> pure (pi, t, b)
+          (Free (M _) ::: _) Value.:$ _ -> do
+            (mA, _A) <- exists Value.Type
+            (_, _B) <- exists _A
+            let _B' = bind mA _B
+            span <- ask
+            (More, _A, _B') <$ tell (Set.singleton (t :===: Value.Pi Ex More _A _B' :@ Assert span))
+          _ -> throwElabError (IllegalApplication t)
+
+        lookupVar (m :.: n) = asks (Scope.lookup (m :.: n)) >>= maybe (throwElabError (FreeVariable (m :.: n))) (pure . entryType)
+        lookupVar (Local n) = asks (Context.lookup n)       >>= maybe (throwElabError (FreeVariable (Local n))) pure
+
+check :: (Carrier sig m, Member (Error ElabError) sig, Member Fresh sig, Member (Reader Context) sig, Member (Reader Gensym) sig, Member (Reader Scope) sig, Member (Reader Span) sig, Member (Reader Usage) sig, Member (Writer Resources) sig, Member (Writer (Set.Set (Caused (Equation Value)))) sig, Monad m)
       => Typed Core.Core
       -> m (Typed Value)
-check tm = send (Check tm ret)
+check = \case
+  tm ::: ty@(Value.Pi Im pi t b) -> gensym "" >>= \ n -> censor (Resources.delete (qlocal n)) $ do
+    (res, e' ::: _) <- n ::: t |- listen (check (tm ::: instantiate (free (qlocal n ::: t)) b))
+    verifyResources tm n pi res
+    pure (Value.lam (qlocal n ::: t) e' ::: ty)
+  Core.Lam e ::: ty@(Value.Pi Ex pi t b) -> gensym "" >>= \ n -> censor (Resources.delete (qlocal n)) $ do
+    (res, e' ::: _) <- n ::: t |- listen (check (Core.instantiate (Core.free (Local n)) e ::: instantiate (free (qlocal n ::: t)) b))
+    verifyResources (Core.Lam e) n pi res
+    pure (Value.lam (qlocal n ::: t) e' ::: ty)
+  Core.Hole _ ::: ty -> do
+    (_, m) <- exists ty
+    pure (m ::: ty)
+  Core.Ann ann tm ::: ty -> local (const ann) (check (tm ::: ty))
+  tm ::: ty@((Free (Q (_ :.: _)) ::: _) Value.:$ _) -> do
+   ty' <- whnf ty
+   check (tm ::: ty')
+  tm ::: ty -> do
+    tm' ::: inferred <- infer tm
+    unified <- unify Value.Type (ty :===: inferred)
+    pure (tm' ::: unified)
+  where verifyResources tm n pi br = do
+          let used = Resources.lookup (qlocal n) br
+          sigma <- ask
+          unless (sigma >< pi == More) . when (pi /= used) $
+            throwElabError (ResourceMismatch n pi used (Core.uses n tm))
+
+        unify ty (tm1 :===: tm2) = if tm1 == tm2 then pure tm1 else do
+          (_, v) <- exists ty
+          span <- ask
+          v <$ tell (Set.fromList [ v :===: tm1 :@ Assert span
+                                  , v :===: tm2 :@ Assert span ])
+
+(|-) :: (Carrier sig m, Member (Reader Context) sig) => Typed Gensym -> m a -> m a
+n ::: t |- m = local (Context.insert (n ::: t)) m
+
+infix 5 |-
+
+throwElabError :: (Carrier sig m, Member (Error ElabError) sig, Member (Reader Context) sig, Member (Reader Span) sig, Monad m) => ErrorReason -> m a
+throwElabError reason = ElabError <$> ask <*> ask <*> pure reason >>= throwError
+
+exists :: (Carrier sig m, Member Fresh sig, Member (Reader Context) sig, Member (Reader Gensym) sig, Monad m) => Type -> m (MName, Type)
+exists t = do
+  Context c <- ask
+  n <- M . Meta <$> gensym "_meta_"
+  pure (n, free (n ::: lams (fmap (fmap qlocal) c) t) $$* fmap (free . fmap qlocal) c)
 
 
 type ModuleTable = Map.Map ModuleName Scope
