c82f172f63e96d64718b8d40a17ce5c5282907c2
Rob Rix <rob.rix@me.com>
2019-01-19 02:45:57 -0500

    Define some machinery for constructing Pos/Span from Trifecta’s types.

--- src/Path/Span.hs
+++ src/Path/Span.hs
@@ -1,10 +1,19 @@
 module Path.Span where
 
+import qualified Text.Trifecta.Delta as Trifecta
+import qualified Text.Trifecta.Rendering as Trifecta
+
 data Pos = Pos
   { posLine :: {-# UNPACK #-} !Int
   , posCol  :: {-# UNPACK #-} !Int
   }
   deriving (Eq, Ord, Show)
+
+fromDelta :: Trifecta.Delta -> Pos
+fromDelta d = Pos (fromIntegral (line d)) (fromIntegral (Trifecta.column d))
+  where line (Trifecta.Lines      l _ _ _) = l
+        line (Trifecta.Directed _ l _ _ _) = l
+        line _                             = 1
 
 data Span = Span
   { spanStart :: {-# UNPACK #-} !Pos
@@ -14,3 +23,6 @@ data Span = Span
 
 instance Semigroup Span where
   Span start1 end1 <> Span start2 end2 = Span (min start1 start2) (max end1 end2)
+
+fromSpan :: Trifecta.Span -> Span
+fromSpan (Trifecta.Span d1 d2 _) = Span (fromDelta d1) (fromDelta d2)
