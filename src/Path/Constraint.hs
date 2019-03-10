75bc6e91ff09d3b668692f87059d387861b72a29
Rob Rix <robrix@github.com>
2019-03-09 20:38:40 -0500

    Generalize the Substitutable instance for :::.

--- src/Path/Constraint.hs
+++ src/Path/Constraint.hs
@@ -78,7 +78,7 @@ instance Substitutable Value where
 instance Substitutable a => Substitutable (Equation a) where
   apply subst (s1 :===: s2) = apply subst s1 :===: apply subst s2
 
-instance Substitutable a => Substitutable (a ::: Type) where
+instance (Substitutable a, Substitutable b) => Substitutable (a ::: b) where
   apply subst (tm ::: ty) = apply subst tm ::: apply subst ty
 
 instance (Ord a, Substitutable a) => Substitutable (Set.Set a) where
