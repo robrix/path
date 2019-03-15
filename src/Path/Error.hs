5ac54af4dc93460481487ae29e93582dd1c70735
Rob Rix <robrix@github.com>
2019-03-15 02:59:49 -0400

    :fire: TypeMismatch errors.

--- src/Path/Error.hs
+++ src/Path/Error.hs
@@ -1,15 +1,12 @@
 {-# LANGUAGE TypeOperators #-}
 module Path.Error where
 
-import           Data.Foldable (fold, toList)
-import qualified Data.Set as Set
-import           Path.Constraint
-import           Path.Context as Context
-import           Path.Name
-import           Path.Pretty
-import           Path.Usage
-import           Path.Value
-import           Text.Trifecta.Rendering (Span)
+import Path.Context as Context
+import Path.Name
+import Path.Pretty
+import Path.Usage
+import Path.Value
+import Text.Trifecta.Rendering (Span)
 
 data ElabError = ElabError
   { errorSpan    :: Span
@@ -20,7 +17,6 @@ data ElabError = ElabError
 
 data ErrorReason
   = FreeVariable Qual
-  | TypeMismatch (Set.Set (Caused (Equation (Value Meta) ::: Type Meta)))
   | IllegalApplication (Type Meta)
   | ResourceMismatch Gensym Usage Usage [Span]
   | TypedHole Qual (Type Meta)
@@ -30,8 +26,6 @@ data ErrorReason
 instance Pretty ElabError where
   pretty (ElabError span ctx reason) = case reason of
     FreeVariable name -> prettyErr span (pretty "free variable" <+> squotes (pretty name)) (prettyCtx ctx)
-    TypeMismatch eqns -> prettyErr span (fold (punctuate hardline
-      (pretty "type mismatch" : map prettyEqn (toList eqns)))) (prettyCtx ctx)
     IllegalApplication ty -> prettyErr span (pretty "illegal application of term of type" <+> pretty ty) (prettyCtx ctx)
     ResourceMismatch n pi used uses -> prettyErr span msg (prettyCtx ctx <> map prettys uses)
       where msg = pretty "Variable" <+> squotes (pretty n) <+> pretty "used" <+> pretty (if pi > used then "less" else "more") <+> parens (pretty (length uses)) <+> pretty "than required" <+> parens (pretty pi)
@@ -39,12 +33,5 @@ instance Pretty ElabError where
       where msg = pretty "Found hole" <+> squotes (pretty n) <+> pretty "of type" <+> squotes (pretty ty)
     InfiniteType n t -> prettyErr span (pretty "Cannot construct infinite type" <+> pretty n <+> blue (pretty "~") <+> pretty t) (prettyCtx ctx)
     where prettyCtx ctx = if null ctx then [] else [nest 2 (vsep [pretty "Local bindings:", pretty ctx])]
-          prettyEqn ((expected :===: actual) ::: _ :@ cause) = fold (punctuate hardline
-            ( pretty "expected:" <+> pretty expected
-            : pretty "  actual:" <+> pretty actual
-            : prettyCause cause))
-          prettyCause (Assert span) = [magenta (pretty "via source"), prettys span]
-          prettyCause (Via eqn cause) = magenta (pretty "via") <+> pretty eqn : prettyCause cause
-          prettyCause (l :<>: r) = prettyCause l <> prettyCause r
 
 instance PrettyPrec ElabError
