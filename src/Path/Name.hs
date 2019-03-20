f6078abee12ce7e446f933edce115b6c39c218fa
Rob Rix <robrix@github.com>
2019-03-20 09:09:01 -0400

    Correct a whitespace error with prefix operators.

--- src/Path/Name.hs
+++ src/Path/Name.hs
@@ -175,7 +175,7 @@ showOperator = renderOperator " " id
 
 renderOperator :: Monoid m => m -> (String -> m) -> Operator -> m
 renderOperator space pretty = \case
-  Prefix (f:|fs) -> pretty f <+> underscore <+> hsep (map (\ a -> pretty a <+> underscore) fs)
+  Prefix (f:|fs) -> pretty f <+> hsep (underscore : map (\ a -> pretty a <+> underscore) fs)
   Postfix (f:|fs) -> underscore <+> pretty f <+> hsep (map (\ a -> underscore <+> pretty a) fs)
   Infix (f:|fs) -> underscore <+> pretty f <+> hsep (underscore : map (\ a -> pretty a <+> underscore) fs)
   Closed fs ff -> foldr (\ a rest -> pretty a <+> underscore <+> rest) (pretty ff) fs
