36ebe231b4db71ac5947574d8d45fd29c098c416
Rob Rix <robrix@github.com>
2019-03-20 19:35:54 -0400

    Define a helper to throw a Doc error for free variables.

--- src/Path/Error.hs
+++ src/Path/Error.hs
@@ -1,6 +1,9 @@
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Path.Error where
 
+import Control.Effect
+import Control.Effect.Error
+import Control.Effect.Reader
 import Path.Name
 import Path.Pretty
 import Text.Trifecta.Rendering (Span)
@@ -20,3 +23,8 @@ instance Pretty ElabError where
     FreeVariable name -> prettyErr span (pretty "free variable" <+> squotes (pretty name)) []
 
 instance PrettyPrec ElabError
+
+freeVariable :: (Carrier sig m, Member (Error Doc) sig, Member (Reader Span) sig, Pretty name) => name -> m a
+freeVariable name = do
+  span <- ask
+  throwError (prettyErr span (pretty "free variable" <+> squotes (pretty name)) [])
