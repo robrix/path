9a19c331e4ea624a8ac501bd8d37ae7b83c08e50
Rob Rix <robrix@github.com>
2019-03-20 09:10:16 -0400

    Correct a whitespace error with postfix operators.

--- src/Path/Name.hs
+++ src/Path/Name.hs
@@ -176,7 +176,7 @@ showOperator = renderOperator " " id
 renderOperator :: Monoid m => m -> (String -> m) -> Operator -> m
 renderOperator space pretty = \case
   Prefix (f:|fs) -> pretty f <+> hsep (underscore : map (\ a -> pretty a <+> underscore) fs)
-  Postfix (f:|fs) -> underscore <+> pretty f <+> hsep (map (\ a -> underscore <+> pretty a) fs)
+  Postfix (f:|fs) -> hsep (map (\ a -> underscore <+> pretty a) (f:fs))
   Infix (f:|fs) -> underscore <+> pretty f <+> hsep (underscore : map (\ a -> pretty a <+> underscore) fs)
   Closed fs ff -> foldr (\ a rest -> pretty a <+> underscore <+> rest) (pretty ff) fs
   where hsep []     = mempty
