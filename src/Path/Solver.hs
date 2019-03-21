208108482953f8c587be374f3c367531a882d369
Rob Rix <robrix@github.com>
2019-03-21 00:54:42 -0400

    Move simplify to the top.

--- src/Path/Solver.hs
+++ src/Path/Solver.hs
@@ -25,6 +25,72 @@ import           Text.Trifecta.Rendering (Spanned(..))
 type Blocked = Set.Set HomConstraint
 type Queue = Seq.Seq HomConstraint
 
+-- FIXME: we need constraint dependencies to ensure that we e.g. δ-reduce a type like Either L R and solve the π type unification constraint before we try to solve whatever we typed using it
+
+simplify :: ( Carrier sig m
+            , Effect sig
+            , Member (Error Doc) sig
+            , Member Naming sig
+            , Member (Reader Scope) sig
+            , Member (State Blocked) sig
+            , Member (State Queue) sig
+            , Member (State Substitution) sig
+            )
+         => HomConstraint
+         -> m (Set.Set HomConstraint)
+simplify (constraint :~ span) = ask >>= \ scope -> execWriter (go scope constraint)
+  where go scope = \case
+          _ :|-: (tm1 :===: tm2) ::: _ | tm1 == tm2 -> pure ()
+          ctx :|-: (Pi p1 _ t1 b1 :===: Pi p2 _ t2 b2) ::: Type
+            | p1 == p2 -> do
+              go scope (ctx :|-: (t1 :===: t2) ::: Type)
+              n <- gensym "pi"
+              -- FIXME: this should insert some sort of dependency
+              go scope (Context.insert (n ::: t1) ctx :|-: (Value.instantiate (pure (qlocal n)) b1 :===: Value.instantiate (pure (qlocal n)) b2) ::: Type)
+          ctx :|-: (Pi Im _ t1 b1 :===: tm2) ::: Type -> do
+            n <- exists t1
+            go scope (ctx :|-: (Value.instantiate n b1 :===: tm2) ::: Type)
+          ctx :|-: (tm1 :===: Pi Im _ t2 b2) ::: Type -> do
+            n <- exists t2
+            go scope (ctx :|-: (tm1 :===: Value.instantiate n b2) ::: Type)
+          ctx :|-: (Lam f1 :===: Lam f2) ::: Pi _ _ t b -> do
+            n <- gensym "lam"
+            go scope (Context.insert (n ::: t) ctx :|-: (Value.instantiate (pure (qlocal n)) f1 :===: Value.instantiate (pure (qlocal n)) f2) ::: Value.instantiate (pure (qlocal n)) b)
+          ctx :|-: (f1@(Name (Global _)) :$ sp1 :===: f2@(Name (Global _)) :$ sp2) ::: ty
+            | Just t1 <- whnf scope (f1 :$ sp1)
+            , Just t2 <- whnf scope (f2 :$ sp2) -> do
+              go scope (ctx :|-: (t1 :===: t2) ::: ty)
+          ctx :|-: (f1@(Name (Global _)) :$ sp1 :===: t2) ::: ty
+            | Just t1 <- whnf scope (f1 :$ sp1) -> do
+              go scope (ctx :|-: (t1 :===: t2) ::: ty)
+          ctx :|-: (t1 :===: f2@(Name (Global _)) :$ sp2) ::: ty
+            | Just t2 <- whnf scope (f2 :$ sp2) -> do
+              go scope (ctx :|-: (t1 :===: t2) ::: ty)
+          ctx :|-: (tm1 :===: Lam b2) ::: ty@(Pi _ _ _ _) -> do
+            n <- gensym "lam"
+            go scope (ctx :|-: (lam (qlocal n) (tm1 $$ pure (qlocal n)) :===: Lam b2) ::: ty)
+          ctx :|-: (Lam b1 :===: tm2) ::: ty@(Pi _ _ _ _) -> do
+            n <- gensym "lam"
+            go scope (ctx :|-: (Lam b1 :===: lam (qlocal n) (tm2 $$ pure (qlocal n))) ::: ty)
+          c@(_ :|-: (t1 :===: t2) ::: _)
+            | Just (m, sp) <- pattern t1 -> solve m (Value.lams sp t2)
+            | Just (m, sp) <- pattern t2 -> solve m (Value.lams sp t1)
+            | blocked t1 || blocked t2 -> tell (Set.singleton (c :~ span))
+            | otherwise                -> unsimplifiableConstraint (c :~ span)
+
+        exists _ = pure . Meta <$> gensym ""
+
+        blocked (Meta _ :$ _) = True
+        blocked _             = False
+
+        whnf :: Scope -> Value Meta -> Maybe (Value Meta)
+        whnf scope (Name (Global n) Value.:$ sp) = do
+          entry <- Scope.lookup n scope
+          val <- entryValue entry
+          let val' = weaken val $$* sp
+          maybe (pure val') pure (whnf scope val')
+        whnf _ _ = Nothing
+
 solver :: ( Carrier sig m
           , Effect sig
           , Member (Error Doc) sig
@@ -105,71 +171,6 @@ solve m v = do
 isBlockedOn :: Meta -> HomConstraint -> Bool
 isBlockedOn m = Set.member m . fvs
 
--- FIXME: we need constraint dependencies to ensure that we e.g. δ-reduce a type like Either L R and solve the π type unification constraint before we try to solve whatever we typed using it
-
-simplify :: ( Carrier sig m
-            , Effect sig
-            , Member (Error Doc) sig
-            , Member Naming sig
-            , Member (Reader Scope) sig
-            , Member (State Blocked) sig
-            , Member (State Queue) sig
-            , Member (State Substitution) sig
-            )
-         => HomConstraint
-         -> m (Set.Set HomConstraint)
-simplify (constraint :~ span) = ask >>= \ scope -> execWriter (go scope constraint)
-  where go scope = \case
-          _ :|-: (tm1 :===: tm2) ::: _ | tm1 == tm2 -> pure ()
-          ctx :|-: (Pi p1 _ t1 b1 :===: Pi p2 _ t2 b2) ::: Type
-            | p1 == p2 -> do
-              go scope (ctx :|-: (t1 :===: t2) ::: Type)
-              n <- gensym "pi"
-              -- FIXME: this should insert some sort of dependency
-              go scope (Context.insert (n ::: t1) ctx :|-: (Value.instantiate (pure (qlocal n)) b1 :===: Value.instantiate (pure (qlocal n)) b2) ::: Type)
-          ctx :|-: (Pi Im _ t1 b1 :===: tm2) ::: Type -> do
-            n <- exists t1
-            go scope (ctx :|-: (Value.instantiate n b1 :===: tm2) ::: Type)
-          ctx :|-: (tm1 :===: Pi Im _ t2 b2) ::: Type -> do
-            n <- exists t2
-            go scope (ctx :|-: (tm1 :===: Value.instantiate n b2) ::: Type)
-          ctx :|-: (Lam f1 :===: Lam f2) ::: Pi _ _ t b -> do
-            n <- gensym "lam"
-            go scope (Context.insert (n ::: t) ctx :|-: (Value.instantiate (pure (qlocal n)) f1 :===: Value.instantiate (pure (qlocal n)) f2) ::: Value.instantiate (pure (qlocal n)) b)
-          ctx :|-: (f1@(Name (Global _)) :$ sp1 :===: f2@(Name (Global _)) :$ sp2) ::: ty
-            | Just t1 <- whnf scope (f1 :$ sp1)
-            , Just t2 <- whnf scope (f2 :$ sp2) -> do
-              go scope (ctx :|-: (t1 :===: t2) ::: ty)
-          ctx :|-: (f1@(Name (Global _)) :$ sp1 :===: t2) ::: ty
-            | Just t1 <- whnf scope (f1 :$ sp1) -> do
-              go scope (ctx :|-: (t1 :===: t2) ::: ty)
-          ctx :|-: (t1 :===: f2@(Name (Global _)) :$ sp2) ::: ty
-            | Just t2 <- whnf scope (f2 :$ sp2) -> do
-              go scope (ctx :|-: (t1 :===: t2) ::: ty)
-          ctx :|-: (tm1 :===: Lam b2) ::: ty@(Pi _ _ _ _) -> do
-            n <- gensym "lam"
-            go scope (ctx :|-: (lam (qlocal n) (tm1 $$ pure (qlocal n)) :===: Lam b2) ::: ty)
-          ctx :|-: (Lam b1 :===: tm2) ::: ty@(Pi _ _ _ _) -> do
-            n <- gensym "lam"
-            go scope (ctx :|-: (Lam b1 :===: lam (qlocal n) (tm2 $$ pure (qlocal n))) ::: ty)
-          c@(_ :|-: (t1 :===: t2) ::: _)
-            | Just (m, sp) <- pattern t1 -> solve m (Value.lams sp t2)
-            | Just (m, sp) <- pattern t2 -> solve m (Value.lams sp t1)
-            | blocked t1 || blocked t2 -> tell (Set.singleton (c :~ span))
-            | otherwise                -> unsimplifiableConstraint (c :~ span)
-
-        exists _ = pure . Meta <$> gensym ""
-
-        blocked (Meta _ :$ _) = True
-        blocked _             = False
-
-        whnf :: Scope -> Value Meta -> Maybe (Value Meta)
-        whnf scope (Name (Global n) Value.:$ sp) = do
-          entry <- Scope.lookup n scope
-          val <- entryValue entry
-          let val' = weaken val $$* sp
-          maybe (pure val') pure (whnf scope val')
-        whnf _ _ = Nothing
 
 hetToHom :: HetConstraint -> Set.Set HomConstraint
 hetToHom ((ctx :|-: tm1 ::: ty1 :===: tm2 ::: ty2) :~ span) = Set.fromList
