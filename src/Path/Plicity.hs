ca2b6f0341ea6d4715674f9ba97ed45c3904b0ff
Rob Rix <robrix@github.com>
2019-03-30 08:48:37 -0400

    :fire: the Semgiroup, Monoid, Semiring, and Unital instances for Plicity.

--- src/Path/Plicity.hs
+++ src/Path/Plicity.hs
@@ -1,26 +1,4 @@
 module Path.Plicity where
 
-import Path.Semiring
-
 data Plicity = Im | Ex
   deriving (Eq, Ord, Show)
-
-plicity :: (Eq a, Monoid a) => a -> Plicity
-plicity a | a == zero = zero
-          | otherwise = one
-
-instance Semigroup Plicity where
-  Ex <> _  = Ex
-  _  <> Ex = Ex
-  _  <> _  = Im
-
-instance Monoid Plicity where
-  mempty = Im
-
-instance Semiring Plicity where
-  Im >< _  = Im
-  _  >< Im = Im
-  _  >< _  = Ex
-
-instance Unital Plicity where
-  one = Ex
