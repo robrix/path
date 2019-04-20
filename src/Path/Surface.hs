480df3e9d75f4d576fcc46b6c7cf8179f2318cdc
Rob Rix <robrix@github.com>
2019-04-20 14:02:51 -0400

    :fire: the FreeVariables instance for Surface.

--- src/Path/Surface.hs
+++ src/Path/Surface.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE LambdaCase, MultiParamTypeClasses #-}
 module Path.Surface where
 
-import qualified Data.Set as Set
 import Path.Name
 import Path.Plicity
 import Path.Usage
@@ -16,15 +15,3 @@ data Surface
   | Hole User
   | (Usage, Spanned Surface) :-> Spanned Surface
   deriving (Eq, Ord, Show)
-
-instance FreeVariables User Surface where
-  fvs = \case
-    Var v -> Set.singleton v
-    Lam (_ :< Just v) b -> Set.delete v (fvs b)
-    Lam (_ :< Nothing)  b -> fvs b
-    f :$ (_ :< a) -> fvs f <> fvs a
-    Type -> Set.empty
-    Pi (_ :< (Just v,  _, t)) b -> fvs t <> Set.delete v (fvs b)
-    Pi (_ :< (Nothing, _, t)) b -> fvs t <> fvs b
-    Hole v -> Set.singleton v
-    (_, a) :-> b -> fvs a <> fvs b
