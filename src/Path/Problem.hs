c82d18b4e8705f201b949ef1959468070376c9f0
Rob Rix <robrix@github.com>
2019-04-27 11:34:12 -0400

    Pull equations out into their own datatype.

--- src/Path/Problem.hs
+++ src/Path/Problem.hs
@@ -6,7 +6,7 @@ import Path.Stack
 
 data Problem a
   = Ex (Problem a) (Scope a)
-  | Problem a :===: Problem a
+  | E (Equation (Problem a))
   | Type
   | Lam (Problem a) (Scope a)
   | Pi (Problem a) (Scope a)
@@ -15,3 +15,10 @@ data Problem a
 
 newtype Scope a = Scope (Problem (Incr a))
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+
+data Equation a
+  = a :===: a
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+infix 3 :===:
