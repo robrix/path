86a0d94cd751025ae94c9671f4e2e0fed0c45764
Rob Rix <robrix@github.com>
2019-04-27 11:22:50 -0400

    Define a type of problems.

--- src/Path/Problem.hs
+++ src/Path/Problem.hs
@@ -1,1 +1,15 @@
 module Path.Problem where
+
+import Path.Name
+import Path.Stack
+
+data Problem a
+  = Ex (Problem a) (Scope a)
+  | Problem a ::: Problem a
+  | Problem a :===: Problem a
+  | Type
+  | Lam (Problem a) (Scope a)
+  | Pi (Problem a) (Scope a)
+  | a :$ Stack (Problem a)
+
+newtype Scope a = Scope (Problem (Incr a))
