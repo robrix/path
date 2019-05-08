f5daf4853165426be8cab5d2ca92d1d6dfa17187
Rob Rix <robrix@github.com>
2019-05-08 17:14:07 -0400

    Rename Modify to Map.

--- src/Control/Effect/Stack.hs
+++ src/Control/Effect/Stack.hs
@@ -5,14 +5,14 @@ import Control.Effect.Carrier
 
 data Stack e m k
   = forall a . Push e (m a) (e -> a -> k)
-  | Modify (e -> e) k
+  | Map (e -> e) k
 
 deriving instance Functor (Stack e m)
 
 instance HFunctor (Stack e) where
   hmap f (Push e m k) = Push e (f m) k
-  hmap _ (Modify f k) = Modify f     k
+  hmap _ (Map f    k) = Map f     k
 
 instance Effect (Stack e) where
   handle state handler (Push e m k) = Push e (handler (m <$ state)) (fmap handler . fmap . k)
-  handle state handler (Modify f k) = Modify f (handler (k <$ state))
+  handle state handler (Map f    k) = Map f (handler (k <$ state))
