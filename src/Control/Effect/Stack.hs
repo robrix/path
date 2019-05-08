1b895887277b11ac3e0a3d3963709c9ef18759d6
Rob Rix <robrix@github.com>
2019-05-08 16:44:50 -0400

    Define an HFunctor instance for Stack.

--- src/Control/Effect/Stack.hs
+++ src/Control/Effect/Stack.hs
@@ -1,8 +1,14 @@
 {-# LANGUAGE DeriveFunctor, ExistentialQuantification, StandaloneDeriving #-}
 module Control.Effect.Stack where
 
+import Control.Effect.Carrier
+
 data Stack e m k
   = forall a . Push e (m a) (e -> a -> k)
   | Modify (e -> e) k
 
 deriving instance Functor (Stack e m)
+
+instance HFunctor (Stack e) where
+  hmap f (Push e m k) = Push e (f m) k
+  hmap _ (Modify f k) = Modify f     k
