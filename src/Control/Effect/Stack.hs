8c38d22d17938077f849e12546548a6459689638
Rob Rix <robrix@github.com>
2019-05-08 16:43:48 -0400

    Derive a Functor instance for Stack.

--- src/Control/Effect/Stack.hs
+++ src/Control/Effect/Stack.hs
@@ -1,6 +1,8 @@
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, StandaloneDeriving #-}
 module Control.Effect.Stack where
 
 data Stack e m k
   = forall a . Push e (m a) (e -> a -> k)
   | Modify (e -> e) k
+
+deriving instance Functor (Stack e m)
