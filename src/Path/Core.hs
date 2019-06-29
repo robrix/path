7ac05d364a87fa8ddd58844e90b26bc2b4ad05bc
Rob Rix <rob.rix@me.com>
2019-06-29 13:22:38 -0400

    Define efold over Core.

--- src/Path/Core.hs
+++ src/Path/Core.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE DeriveTraversable, LambdaCase, QuantifiedConstraints, RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE DeriveTraversable, LambdaCase, QuantifiedConstraints, RankNTypes, ScopedTypeVariables, TypeOperators #-}
 module Path.Core where
 
 import Control.Monad (ap)
+import Data.Coerce
+import GHC.Generics ((:.:) (..))
 import Path.Name
 import Path.Plicity
 import Path.Usage
@@ -53,3 +55,32 @@ gfold var lam app ty pi hole ann k = go
           Pi m t b -> pi m (go t) (go b)
           Hole a -> hole a
           Ann span a -> ann span (go a)
+
+efold :: forall l m n z b
+      .  ( forall a b . Coercible a b => Coercible (n a) (n b)
+         , forall a b . Coercible a b => Coercible (m a) (m b)
+         )
+      => (forall a . Name (m a) -> n a)
+      -> (forall a . Plicit (Maybe User) -> n (Incr (n a)) -> n a)
+      -> (forall a . n a -> Plicit (n a) -> n a)
+      -> (forall a . n a)
+      -> (forall a . Usage -> n a -> n a -> n a)
+      -> (forall a . Gensym -> n a)
+      -> (forall a . Span -> n a -> n a)
+      -> (forall a . Incr (n a) -> m (Incr (n a)))
+      -> (l b -> m (z b))
+      -> Core (l b)
+      -> n (z b)
+efold var lam app ty pi hole ann k = go
+  where go :: forall l' z' x . (l' x -> m (z' x)) -> Core (l' x) -> n (z' x)
+        go h = \case
+          Var a -> var (h <$> a)
+          Lam p b -> lam p (coerce (go
+                       (coerce (k . fmap (go h))
+                         :: ((Incr :.: Core :.: l') x -> m ((Incr :.: n :.: z') x)))
+                       (coerce b)))
+          f :$ a -> app (go h f) (go h <$> a)
+          Type -> ty
+          Pi m t b -> pi m (go h t) (go h b)
+          Hole a -> hole a
+          Ann loc b -> ann loc (go h b)
