bc43f44807e912b086c52bce10baebe8f3d9a16f
Rob Rix <rob.rix@me.com>
2019-07-01 10:30:35 -0400

    Specialize the Eq/Ord instances for Expr.

--- src/Path/Expr.hs
+++ src/Path/Expr.hs
@@ -4,7 +4,7 @@ module Path.Expr where
 import Control.Monad (ap)
 import Data.Coerce (coerce)
 import Data.Functor.Const (Const (..))
-import Path.Name (Incr (..))
+import Path.Name (Incr (..), flatten)
 
 data Expr a
   = Var a
@@ -16,9 +16,18 @@ data ExprF f a
   | f a :$ f a
   deriving (Foldable, Functor, Traversable)
 
-deriving instance (Eq   a, forall a . Eq   a => Eq   (f a)) => Eq   (ExprF f a)
-deriving instance (Ord  a, forall a . Eq   a => Eq   (f a)
-                         , forall a . Ord  a => Ord  (f a)) => Ord  (ExprF f a)
+instance (Monad f, Eq  a, forall a . Eq  a => Eq  (f a)) => Eq (ExprF f a) where
+  Lam b1   == Lam b2   = flatten b1 == flatten b2
+  f1 :$ a1 == f2 :$ a2 = f1 == f2 && a1 == a2
+  _        == _        = False
+
+instance (Monad f, Ord a, forall a . Eq  a => Eq  (f a)
+                        , forall a . Ord a => Ord (f a)) => Ord (ExprF f a) where
+  Lam b1     `compare` Lam b2     = flatten b1 `compare` flatten b2
+  Lam _      `compare` _          = LT
+  _          `compare` Lam _      = GT
+  (f1 :$ a1) `compare` (f2 :$ a2) = f1 `compare` f2 <> a1 `compare` a2
+
 deriving instance (Show a, forall a . Show a => Show (f a)) => Show (ExprF f a)
 
 instance Applicative Expr where
