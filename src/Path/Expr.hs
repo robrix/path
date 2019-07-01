420b6a5046faa202409d57eb1ac67a926de901a5
Rob Rix <rob.rix@me.com>
2019-07-01 11:07:56 -0400

    Use Scope in Expr.

--- src/Path/Expr.hs
+++ src/Path/Expr.hs
@@ -4,7 +4,7 @@ module Path.Expr where
 import Control.Monad (ap)
 import Data.Coerce (coerce)
 import Data.Functor.Const (Const (..))
-import Path.Name (Incr (..), flatten)
+import Path.Name (Incr (..), Scope (..))
 
 data Expr a
   = Var a
@@ -12,23 +12,14 @@ data Expr a
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 data ExprF f a
-  = Lam (f (Incr (f a)))
+  = Lam (Scope f a)
   | f a :$ f a
   deriving (Foldable, Functor, Traversable)
 
-instance (Monad f, Eq  a, forall a . Eq  a => Eq  (f a)) => Eq (ExprF f a) where
-  Lam b1   == Lam b2   = flatten b1 == flatten b2
-  f1 :$ a1 == f2 :$ a2 = f1 == f2 && a1 == a2
-  _        == _        = False
-
-instance (Monad f, Ord a, forall a . Eq  a => Eq  (f a)
-                        , forall a . Ord a => Ord (f a)) => Ord (ExprF f a) where
-  Lam b1     `compare` Lam b2     = flatten b1 `compare` flatten b2
-  Lam _      `compare` _          = LT
-  _          `compare` Lam _      = GT
-  (f1 :$ a1) `compare` (f2 :$ a2) = f1 `compare` f2 <> a1 `compare` a2
-
-deriving instance (Show a, forall a . Show a => Show (f a)) => Show (ExprF f a)
+deriving instance (Eq   a, forall a . Eq   a => Eq   (f a), Monad f) => Eq   (ExprF f a)
+deriving instance (Ord  a, forall a . Eq   a => Eq   (f a)
+                         , forall a . Ord  a => Ord  (f a), Monad f) => Ord  (ExprF f a)
+deriving instance (Show a, forall a . Show a => Show (f a))          => Show (ExprF f a)
 
 instance Applicative Expr where
   pure = Var
@@ -49,7 +40,7 @@ eiter var alg k = go
   where go :: forall x y . (x -> m y) -> Expr x -> n y
         go h = \case
           Var a -> var (h a)
-          Expr (Lam b) -> alg (Lam (go (k . fmap (go h)) b))
+          Expr (Lam (Scope b)) -> alg (Lam (Scope (go (k . fmap (go h)) b)))
           Expr (f :$ a) -> alg (go h f :$ go h a)
 
 ecata :: (forall a . ExprF m a -> m a)
