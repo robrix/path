fd12d2963e4065b747f7323c6449ee55286cf96f
Rob Rix <rob.rix@me.com>
2019-07-03 11:24:52 -0400

    Define a helper to construct a Spanned value using a Reader Span effect.

--- src/Path/Span.hs
+++ src/Path/Span.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Path.Span
 ( Span(..)
 , Spanned(..)
 , unSpanned
 , runSpanned
+, spanned
 ) where
 
 import Control.Effect
@@ -14,3 +16,6 @@ unSpanned (a :~ _) = a
 
 runSpanned :: Carrier sig m => (a -> ReaderC Span m b) -> Spanned a -> m (Spanned b)
 runSpanned f v@(_ :~ s) = runReader s (traverse f v)
+
+spanned :: (Carrier sig m, Member (Reader Span) sig) => a -> m (Spanned a)
+spanned a = asks (a :~)
