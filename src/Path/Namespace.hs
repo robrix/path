9bd779098d5866f9fa0b6e691372195f4bf951f2
Rob Rix <rob.rix@me.com>
2019-07-03 09:29:18 -0400

    Derive Foldable & Functor instances for Entry.

--- src/Path/Namespace.hs
+++ src/Path/Namespace.hs
@@ -1,8 +1,11 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, TypeOperators #-}
+{-# LANGUAGE DerivingVia, GeneralizedNewtypeDeriving, TypeOperators #-}
 module Path.Namespace where
 
 import Control.Arrow ((***))
+import Data.Bifoldable
+import Data.Bifunctor
 import Data.Coerce
+import Data.Functor.Identity
 import qualified Data.Map as Map
 import Path.Core
 import Path.Name
@@ -10,6 +13,7 @@ import Path.Pretty
 
 newtype Entry a = Entry { unEntry :: Maybe a ::: a }
   deriving (Eq, Ord, Show)
+  deriving (Foldable, Functor) via (Bi (:::) Maybe Identity)
 
 entryType :: Entry a -> a
 entryType = typedType . unEntry
@@ -45,3 +49,13 @@ under = coerce
 
 instance Pretty Namespace where
   pretty = tabulate2 space . map (green . pretty *** align . group . pretty) . Map.toList . unNamespace
+
+
+-- | A functor composing two functors on the inside of a bifunctor. Can be used with @-XDerivingVia@ to derive 'Foldable', 'Functor', and 'Traversable' instances given 'Bifoldable', 'Bifunctor', and 'Bitraversable' instances for @p@ respectively.
+newtype Bi p f g a = Bi { unBi :: p (f a) (g a) }
+
+instance (Bifoldable p, Foldable f, Foldable g) => Foldable (Bi p f g) where
+  foldMap f = bifoldMap (foldMap f) (foldMap f) . unBi
+
+instance (Bifunctor p, Functor f, Functor g) => Functor (Bi p f g) where
+  fmap f = Bi . bimap (fmap f) (fmap f) . unBi
