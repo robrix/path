c9da92272f31d749c8b79c68e82198800744e458
Rob Rix <rob.rix@me.com>
2019-07-03 21:20:20 -0400

    Parenthesize the signatures for symmetry.

--- src/Control/Monad/Module.hs
+++ src/Control/Monad/Module.hs
@@ -4,8 +4,8 @@ module Control.Monad.Module where
 class (Functor f, Monad m) => RModule f m where
   (>>==) :: f a -> (a -> m b) -> f b
 
-(>==>) :: RModule f m => (a -> f b) -> (b -> m c) -> a -> f c
+(>==>) :: RModule f m => (a -> f b) -> (b -> m c) -> (a -> f c)
 f >==> g = \x -> f x >>== g
 
-(<==<) :: RModule f m => (b -> m c) -> (a -> f b) -> a -> f c
+(<==<) :: RModule f m => (b -> m c) -> (a -> f b) -> (a -> f c)
 g <==< f = \x -> f x >>== g
