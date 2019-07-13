b772034d03d65e136978a75f364151f503c7040f
Rob Rix <rob.rix@me.com>
2019-07-13 18:48:45 -0400

    Define a Syntax typeclass.

--- src/Path/Term.hs
+++ src/Path/Term.hs
@@ -1,5 +1,17 @@
+{-# LANGUAGE RankNTypes #-}
 module Path.Term where
+
+import Control.Effect.Carrier
+import Path.Scope
 
 data Term sig a
   = Var a
   | Term (sig (Term sig) a)
+
+
+class HFunctor sig => Syntax sig where
+  foldSyntax :: (forall x y . (x -> m y) -> f x -> n y)
+             -> (forall a . Incr () (n a) -> m (Incr () (n a)))
+             -> (a -> m b)
+             -> sig f a
+             -> sig n b
