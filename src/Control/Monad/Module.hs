2c09108d81b370edf6f05fbc8ce319844cd84b71
Rob Rix <rob.rix@me.com>
2019-07-13 11:40:53 -0400

    Give fixities for all the module operators.

--- src/Control/Monad/Module.hs
+++ src/Control/Monad/Module.hs
@@ -5,12 +5,17 @@ import GHC.Generics
 
 class (Functor f, Monad m) => RModule f m where
   (>>=*) :: f a -> (a -> m b) -> f b
+  infixl 1 >>=*
 
 (>=>*) :: RModule f m => (a -> f b) -> (b -> m c) -> (a -> f c)
 f >=>* g = \x -> f x >>=* g
 
+infixl 1 >=>*
+
 (<=<*) :: RModule f m => (b -> m c) -> (a -> f b) -> (a -> f c)
 g <=<* f = \x -> f x >>=* g
+
+infixl 1 <=<*
 
 joinr :: RModule f m => f (m a) -> f a
 joinr = (>>=* id)
@@ -22,12 +27,17 @@ instance (Functor f, RModule g h) => RModule (f :.: g) h where
 
 class (Functor f, Monad m) => LModule m f where
   (*>>=) :: m a -> (a -> f b) -> f b
+  infixl 1 *>>=
 
 (*>=>) :: LModule m f => (a -> m b) -> (b -> f c) -> (a -> f c)
 f *>=> g = \x -> f x *>>= g
 
+infixl 1 *>=>
+
 (*<=<) :: LModule m f => (b -> f c) -> (a -> m b) -> (a -> f c)
 g *<=< f = \x -> f x *>>= g
+
+infixl 1 *<=<
 
 joinl :: LModule m f => m (f a) -> f a
 joinl = (*>>= id)
