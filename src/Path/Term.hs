bd5870f48481cfd015274642deac217ca0ff80fa
Rob Rix <rob.rix@me.com>
2019-07-13 11:16:24 -0400

    Derive some instances.

--- src/Path/Term.hs
+++ src/Path/Term.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE StandaloneDeriving, UndecidableInstances #-}
 module Path.Term where
 
 data Term f a
   = Var a
   | Term (f (Term f) a)
+
+deriving instance (Eq   a, Eq   (f (Term f) a)) => Eq   (Term f a)
+deriving instance (Ord  a, Ord  (f (Term f) a)) => Ord  (Term f a)
+deriving instance (Show a, Show (f (Term f) a)) => Show (Term f a)
