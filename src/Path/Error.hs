5441df470ad4fd195d274facc92570c7b7da76c4
Rob Rix <rob.rix@me.com>
2019-07-14 11:31:22 -0400

    :fire: unsimplifiableConstraints.

--- src/Path/Error.hs
+++ src/Path/Error.hs
@@ -7,10 +7,7 @@ import Control.Effect.Reader
 import Data.Foldable (fold, toList)
 import Data.List (intersperse)
 import Data.List.NonEmpty (NonEmpty(..))
-import qualified Data.Map as Map
 import qualified Data.Set as Set
-import Path.Core
-import Path.Constraint
 import Path.Name
 import Path.Pretty
 import Path.Span (Span, Spanned(..))
@@ -26,12 +23,6 @@ ambiguousName name sources = do
   throwError (prettyErr span (pretty "ambiguous name" <+> squotes (pretty name)) [nest 2 (vsep
     ( pretty "it could refer to"
     : map pretty (toList sources)))])
-
-
-unsimplifiableConstraints :: (Carrier sig m, Member (Error Doc) sig) => Signature -> [Spanned (Constraint Core (Name Meta))] -> m a
-unsimplifiableConstraints sig constraints = throwError (fold (intersperse hardline (map unsimplifiable constraints)))
-  where unsimplifiable (c :~ span) = prettyErr span (pretty "unsimplifiable constraint") [pretty (sigFor c) <> pretty c]
-        sigFor c = let fvs' = metaNames (localNames (fvs c)) in Signature (Map.filterWithKey (\ k _ -> k `Set.member` fvs') (unSignature sig))
 
 
 unknownModule :: (Carrier sig m, Member (Error Doc) sig) => Spanned ModuleName -> m a
