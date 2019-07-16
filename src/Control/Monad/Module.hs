df2bdf4eebe858ebad4ff9e81669ad490d4d6495
Rob Rix <rob.rix@me.com>
2019-07-16 09:44:03 -0400

    Define an HRModule instance for sums.

--- src/Control/Monad/Module.hs
+++ src/Control/Monad/Module.hs
@@ -2,7 +2,7 @@
 module Control.Monad.Module where
 
 import Control.Effect.Carrier
-import GHC.Generics
+import GHC.Generics ((:.:) (..))
 
 class (Functor f, Monad m) => RModule f m where
   (>>=*) :: f a -> (a -> m b) -> f b
@@ -47,3 +47,7 @@ joinl = (*>>= id)
 class (forall g . Functor g => Functor (f g), HFunctor f) => HRModule f where
   (>>=**) :: Monad m => f m a -> (a -> m b) -> f m b
   infixl 1 >>=**
+
+instance (HRModule f, HRModule g) => HRModule (f :+: g) where
+  L l >>=** f = L (l >>=** f)
+  R r >>=** f = R (r >>=** f)
