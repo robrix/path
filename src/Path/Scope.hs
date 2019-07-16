a46fa941b988d1238a59bd61f010ae3783efd0e8
Rob Rix <rob.rix@me.com>
2019-07-16 10:10:37 -0400

    Define a ScopeT transformer.

--- src/Path/Scope.hs
+++ src/Path/Scope.hs
@@ -172,3 +172,61 @@ fromScopeH = unScopeH >=>* sequenceA
 
 toScopeH :: (Functor f, Applicative g) => f (Incr a b) -> ScopeH a f g b
 toScopeH = ScopeH . fmap (fmap pure)
+
+
+-- | Like 'Scope', but allows the inner functor to vary. Useful for syntax like declaration scopes, case alternatives, etc., which can bind variables, but cannot (directly) consist solely of them.
+newtype ScopeT a t f b = ScopeT (t f (Incr a (f b)))
+  deriving (Foldable, Functor, Generic1, Traversable)
+
+unScopeT :: ScopeT a t f b -> t f (Incr a (f b))
+unScopeT (ScopeT s) = s
+
+instance (HRModule t, Monad f, Eq  a, Eq  b, forall a . Eq  a => Eq  (t f a)) => Eq  (ScopeT a t f b) where
+  (==) = (==) `on` fromScopeT
+
+instance (HRModule t, Monad f, Ord a, Ord b, forall a . Eq  a => Eq  (t f a)
+                                           , forall a . Ord a => Ord (t f a)) => Ord (ScopeT a t f b) where
+  compare = compare `on` fromScopeT
+
+deriving instance (Show a, Show b, forall a . Show a => Show (t f a)
+                                 , forall a . Show a => Show (f a)) => Show (ScopeT a t f b)
+
+instance (Applicative (t f), Applicative f) => Applicative (ScopeT a t f) where
+  pure = ScopeT . pure . S . pure
+  ScopeT f <*> ScopeT a = ScopeT (liftA2 (liftA2 (<*>)) f a)
+
+instance (HFunctor t, forall g . Functor g => Functor (t g)) => HRModule (ScopeT b t) where
+  ScopeT s >>=** k = ScopeT (fmap (>>= k) <$> s)
+
+instance MonadTrans f => MonadTrans (ScopeT a f) where
+  lift = ScopeT . lift . pure . S
+
+instance (HFunctor t, forall g . Functor g => Functor (t g)) => HFunctor (ScopeT a t) where
+  hmap f = ScopeT . hmap f . fmap (fmap f) . unScopeT
+
+
+-- | Bind occurrences of a variable in a term, producing a term in which the variable is bound.
+bind1T :: (Functor (t f), Applicative f, Eq a) => a -> t f a -> ScopeT () t f a
+bind1T n = bindT (guard . (== n))
+
+bindT :: (Functor (t f), Applicative f) => (b -> Maybe a) -> t f b -> ScopeT a t f b
+bindT f = bindTEither (matchMaybe f)
+
+bindTEither :: (Functor (t f), Applicative f) => (b -> Either a c) -> t f b -> ScopeT a t f c
+bindTEither f = ScopeT . fmap (match f) -- FIXME: succ as little of the expression as possible, cf https://twitter.com/ollfredo/status/1145776391826358273
+
+-- | Substitute a term for the free variable in a given term, producing a closed term.
+instantiate1T :: (HRModule t, Monad f) => f b -> ScopeT a t f b -> t f b
+instantiate1T t = instantiateT (const t)
+
+instantiateT :: (HRModule t, Monad f) => (a -> f b) -> ScopeT a t f b -> t f b
+instantiateT f = instantiateTEither (either f pure)
+
+instantiateTEither :: (HRModule t, Monad f) => (Either a b -> f c) -> ScopeT a t f b -> t f c
+instantiateTEither f = unScopeT >=>** incr (f . Left) (>>= f . Right)
+
+fromScopeT :: (HRModule t, Monad f) => ScopeT a t f b -> t f (Incr a b)
+fromScopeT = unScopeT >=>** sequenceA
+
+toScopeT :: (Functor (t f), Applicative f) => t f (Incr a b) -> ScopeT a t f b
+toScopeT = ScopeT . fmap (fmap pure)
