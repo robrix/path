5218e3d667841eed4cfdb04d9b7ca6afc4c83d6b
Rob Rix <rob.rix@me.com>
2019-07-20 12:49:46 -0400

    Define a Lexer effect.

--- src/Path/Lexer.hs
+++ src/Path/Lexer.hs
@@ -1,1 +1,8 @@
+{-# LANGUAGE DeriveFunctor, DeriveGeneric #-}
 module Path.Lexer where
+
+import GHC.Generics (Generic1)
+
+data Lexer m k
+  = Satisfy (Char -> Bool) (Bool -> m k)
+  deriving (Functor, Generic1)
