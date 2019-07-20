fc1402b7252f277b0027402865886d03bca8e44c
Rob Rix <rob.rix@me.com>
2019-07-20 13:05:42 -0400

    Add an eof constructor.

--- src/Path/Lexer.hs
+++ src/Path/Lexer.hs
@@ -5,14 +5,17 @@ import Control.Effect.Carrier
 
 data Lexer m k
   = forall a . Accept (Char -> Maybe a) (a -> m k)
+  | Eof (m k)
 
 deriving instance Functor m => Functor (Lexer m)
 
 instance HFunctor Lexer where
   hmap f (Accept p k) = Accept p (f . k)
+  hmap f (Eof      k) = Eof (f k)
 
 instance Effect Lexer where
   handle state handler (Accept p k) = Accept p (handler . (<$ state) . k)
+  handle state handler (Eof      k) = Eof      (handler . (<$ state) $ k)
 
 
 accept :: (Carrier sig m, Member Lexer sig) => (Char -> Maybe a) -> m a
@@ -20,6 +23,10 @@ accept p = send (Accept p pure)
 
 satisfy :: (Carrier sig m, Member Lexer sig) => (Char -> Bool) -> m Char
 satisfy p = accept (\ c -> if p c then Just c else Nothing)
+
+
+eof :: (Carrier sig m, Member Lexer sig) => m ()
+eof = send (Eof (pure ()))
 
 
 data Pos = Pos
