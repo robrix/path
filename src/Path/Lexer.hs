68bcb97ff752f597cbff30e8c12b108fb12a1c7a
Rob Rix <rob.rix@me.com>
2019-07-20 12:50:59 -0400

    Derive HFunctor & Effect instances for Lexer.

--- src/Path/Lexer.hs
+++ src/Path/Lexer.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE DeriveFunctor, DeriveGeneric #-}
 module Path.Lexer where
 
+import Control.Effect.Carrier
 import GHC.Generics (Generic1)
 
 data Lexer m k
   = Satisfy (Char -> Bool) (Bool -> m k)
   deriving (Functor, Generic1)
+
+instance HFunctor Lexer
+instance Effect Lexer
