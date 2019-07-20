a1b08d149d808d07e64bdc67867267a243387fb9
Rob Rix <rob.rix@me.com>
2019-07-20 13:13:52 -0400

    Define a labeling operation.

--- src/Path/Lexer.hs
+++ src/Path/Lexer.hs
@@ -5,17 +5,20 @@ import Control.Effect.Carrier
 
 data Lexer m k
   = forall a . Accept (Char -> Maybe a) (a -> m k)
+  | forall a . Label (m a) String (a -> m k)
   | Eof (m k)
 
 deriving instance Functor m => Functor (Lexer m)
 
 instance HFunctor Lexer where
-  hmap f (Accept p k) = Accept p (f . k)
-  hmap f (Eof      k) = Eof (f k)
+  hmap f (Accept p  k) = Accept p      (f . k)
+  hmap f (Label m s k) = Label (f m) s (f . k)
+  hmap f (Eof       k) = Eof           (f   k)
 
 instance Effect Lexer where
-  handle state handler (Accept p k) = Accept p (handler . (<$ state) . k)
-  handle state handler (Eof      k) = Eof      (handler . (<$ state) $ k)
+  handle state handler (Accept p  k) = Accept p (handler . (<$ state) . k)
+  handle state handler (Label m s k) = Label (handler (m <$ state)) s (handler . fmap k)
+  handle state handler (Eof       k) = Eof      (handler . (<$ state) $ k)
 
 
 accept :: (Carrier sig m, Member Lexer sig) => (Char -> Maybe a) -> m a
@@ -23,6 +26,12 @@ accept p = send (Accept p pure)
 
 satisfy :: (Carrier sig m, Member Lexer sig) => (Char -> Bool) -> m Char
 satisfy p = accept (\ c -> if p c then Just c else Nothing)
+
+
+(<?>) :: (Carrier sig m, Member Lexer sig) => m a -> String -> m a
+m <?> s = send (Label m s pure)
+
+infixr 0 <?>
 
 
 eof :: (Carrier sig m, Member Lexer sig) => m ()
