f96fc62cb92c1a444ffb7fde179b057a7fdebb79
Rob Rix <rob.rix@me.com>
2019-07-20 14:31:15 -0400

    Rename all the Lexer stuff to Parser.

--- src/Path/Lexer.hs
+++ src/Path/Lexer.hs
@@ -10,28 +10,28 @@ import           Text.Parser.Char
 import           Text.Parser.Combinators
 import           Text.Parser.Token
 
-data Lexer m k
+data Parser m k
   = forall a . Accept (Char -> Maybe a) (a -> m k)
   | forall a . Label (m a) String (a -> m k)
   | Unexpected String
   | Eof (m k)
 
-deriving instance Functor m => Functor (Lexer m)
+deriving instance Functor m => Functor (Parser m)
 
-instance HFunctor Lexer where
+instance HFunctor Parser where
   hmap f (Accept p   k) = Accept p      (f . k)
   hmap f (Label m s  k) = Label (f m) s (f . k)
   hmap _ (Unexpected s) = Unexpected s
   hmap f (Eof        k) = Eof           (f   k)
 
-instance Effect Lexer where
+instance Effect Parser where
   handle state handler (Accept p   k) = Accept p (handler . (<$ state) . k)
   handle state handler (Label m s  k) = Label (handler (m <$ state)) s (handler . fmap k)
   handle _     _       (Unexpected s) = Unexpected s
   handle state handler (Eof        k) = Eof      (handler . (<$ state) $ k)
 
 
-accept :: (Carrier sig m, Member Lexer sig) => (Char -> Maybe a) -> m a
+accept :: (Carrier sig m, Member Parser sig) => (Char -> Maybe a) -> m a
 accept p = send (Accept p pure)
 
 
@@ -48,40 +48,40 @@ data Span = Span
   deriving (Eq, Ord, Show)
 
 
-runLexer :: String -> LexerC m a -> m (String, a)
-runLexer s = runState s . runLexerC
+runParser :: String -> ParserC m a -> m (String, a)
+runParser s = runState s . runParserC
 
-newtype LexerC m a = LexerC { runLexerC :: StateC String m a }
+newtype ParserC m a = ParserC { runParserC :: StateC String m a }
   deriving (Alternative, Applicative, Functor, Monad)
 
-instance (Alternative m, Carrier sig m, Effect sig, Member Cut sig) => Parsing (LexerC m) where
+instance (Alternative m, Carrier sig m, Effect sig, Member Cut sig) => Parsing (ParserC m) where
   try = call
   eof = send (Eof (pure ()))
   unexpected s = send (Unexpected s)
   m <?> s = send (Label m s pure)
   notFollowedBy m = m >>= unexpected . show
 
-instance (Alternative m, Carrier sig m, Effect sig, Member Cut sig) => CharParsing (LexerC m) where
+instance (Alternative m, Carrier sig m, Effect sig, Member Cut sig) => CharParsing (ParserC m) where
   satisfy p = accept (\ c -> if p c then Just c else Nothing)
 
-instance (Alternative m, Carrier sig m, Effect sig, Member Cut sig) => TokenParsing (LexerC m)
+instance (Alternative m, Carrier sig m, Effect sig, Member Cut sig) => TokenParsing (ParserC m)
 
-instance (Alternative m, Carrier sig m, Effect sig) => Carrier (Lexer :+: sig) (LexerC m) where
+instance (Alternative m, Carrier sig m, Effect sig) => Carrier (Parser :+: sig) (ParserC m) where
   eff = \case
-    L (Accept p k) -> LexerC $ do
+    L (Accept p k) -> ParserC $ do
       cs <- get @String
       case cs of
-        c:cs | Just a <- p c -> put cs *> runLexerC (k a)
+        c:cs | Just a <- p c -> put cs *> runParserC (k a)
         -- FIXME: error message
         _                    -> empty
     -- FIXME: use the label to provide contextualized error messages
     L (Label m _ k) -> m >>= k
     -- FIXME: fail with an error message
     L (Unexpected _) -> empty
-    L (Eof k) -> LexerC $ do
+    L (Eof k) -> ParserC $ do
       cs <- get @String
       case cs of
-        "" -> runLexerC k
+        "" -> runParserC k
         -- FIXME: error message
         _  -> empty
-    R other -> LexerC (eff (R (handleCoercible other)))
+    R other -> ParserC (eff (R (handleCoercible other)))
