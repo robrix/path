be7fa6c803c202a6c5ae44721f912417c0b078dd
Rob Rix <rob.rix@me.com>
2019-07-22 01:50:35 -0400

    Spacing.

--- src/Path/Span.hs
+++ src/Path/Span.hs
@@ -14,6 +14,7 @@ import Text.Trifecta.Rendering (Span (..), Spanned (..))
 unSpanned :: Spanned a -> a
 unSpanned (a :~ _) = a
 
+
 runSpanned :: Carrier sig m => (a -> ReaderC Span m b) -> Spanned a -> m (Spanned b)
 runSpanned f v@(_ :~ s) = runReader s (traverse f v)
 
