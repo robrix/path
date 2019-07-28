e7bbb15fcb6ece1a0804a2c75e1adca2a2d44b30
Rob Rix <rob.rix@me.com>
2019-07-28 13:37:21 -0400

    Extract the recursive portion of prettyTerm.

--- src/Path/Term.hs
+++ src/Path/Term.hs
@@ -49,8 +49,17 @@ prettyTerm
   => (forall f n . (Foldable f, Monad f) => (forall n . Vec n Doc -> f (Var (Fin n) a) -> Prec) -> Vec n Doc -> sig f (Var (Fin n) a) -> Prec)
   -> Term sig a
   -> Doc
-prettyTerm alg = precDoc . go VZ . fmap F
-  where go :: Vec n Doc -> Term sig (Var (Fin n) a) -> Prec
+prettyTerm alg = precDoc . prettyTermInContext alg VZ . fmap F
+
+prettyTermInContext
+  :: forall sig n a
+  .  (forall g . Foldable g => Foldable (sig g), Pretty a, RightModule sig)
+  => (forall f n . (Foldable f, Monad f) => (forall n . Vec n Doc -> f (Var (Fin n) a) -> Prec) -> Vec n Doc -> sig f (Var (Fin n) a) -> Prec)
+  -> Vec n Doc
+  -> Term sig (Var (Fin n) a)
+  -> Prec
+prettyTermInContext alg = go
+  where go :: forall n . Vec n Doc -> Term sig (Var (Fin n) a) -> Prec
         go ctx = \case
           Var v -> atom (var (pretty . (ctx !)) pretty v)
           Term t -> alg go ctx t
