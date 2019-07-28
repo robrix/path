2fcbe48d7fca338d06a31cf6b7e687c2827cc17d
Rob Rix <rob.rix@me.com>
2019-07-28 11:07:25 -0400

    Define pretty-printing of Terms using Prec.

--- src/Path/Problem.hs
+++ src/Path/Problem.hs
@@ -10,14 +10,11 @@ import qualified Data.Set as Set
 import           GHC.Generics (Generic1)
 import           Path.Core
 import           Path.Name
-import           Path.Pretty hiding (prec, withPrec)
+import           Path.Pretty
 import           Path.Scope
 import           Path.Syntax
 import           Path.Term
 import           Prelude hiding (pi)
-
-newtype P = P { unP :: Int }
-  deriving (Eq, Ord, Show)
 
 instance Pretty a => Pretty (Term (Problem :+: Core) a) where
   pretty = prettyTerm (\ go -> \case
@@ -26,59 +23,51 @@ instance Pretty a => Pretty (Term (Problem :+: Core) a) where
 
 prettyTerm
   :: forall a syntax . (Pretty a, RightModule syntax)
-  => (forall f sig m . (Carrier sig m, Member (Reader N) sig, Member (Reader P) sig, Member (Writer (Set.Set N)) sig, Monad f) => (f (m Doc) -> m Doc) -> syntax f (m Doc) -> m Doc)
+  => (forall f sig m . (Carrier sig m, Member (Reader N) sig, Member (Writer (Set.Set N)) sig, Monad f) => (f (m Prec) -> m Prec) -> syntax f (m Prec) -> m Prec)
   -> Term syntax a
   -> Doc
-prettyTerm alg = snd . run . runWriter @(Set.Set N) . runReader (P 0) . runReader (N 0) . go . fmap (pure . pretty)
-  where go :: (Carrier sig m, Member (Reader N) sig, Member (Reader P) sig, Member (Writer (Set.Set N)) sig) => Term syntax (m Doc) -> m Doc
+prettyTerm alg = precDoc . snd . run . runWriter @(Set.Set N) . runReader (N 0) . go . fmap (pure . atom . pretty)
+  where go :: (Carrier sig m, Member (Reader N) sig, Member (Writer (Set.Set N)) sig) => Term syntax (m Prec) -> m Prec
         go = \case
           Var v -> v
           Term t -> alg go t
 
-prec :: (Carrier sig m, Member (Reader P) sig) => Int -> Doc -> m Doc
-prec d' doc = do
-  d <- ask
-  pure (prettyParens (d > P d') doc)
-
-withPrec :: (Carrier sig m, Member (Reader P) sig) => Int -> m a -> m a
-withPrec i = local (const (P i))
-
-binding :: (Carrier sig m, Member (Reader N) sig, Member (Writer (Set.Set N)) sig, Monad f) => (f (m Doc) -> m Doc) -> (N -> Doc) -> Scope a f (m Doc) -> m (N, Doc)
+binding :: (Carrier sig m, Member (Reader N) sig, Member (Writer (Set.Set N)) sig, Monad f) => (f (m Prec) -> m Prec) -> (N -> Doc) -> Scope a f (m Prec) -> m (N, Prec)
 binding go pretty m = bindN $ \ n ->
-  (,) n <$> go (instantiate1 (pure (tell (Set.singleton n) *> pure (pretty n))) m)
+  (,) n <$> go (instantiate1 (pure (tell (Set.singleton n) *> pure (atom (pretty n)))) m)
 
-prettyCore :: (Carrier sig m, Member (Reader N) sig, Member (Reader P) sig, Member (Writer (Set.Set N)) sig, Monad f) => (f (m Doc) -> m Doc) -> Core f (m Doc) -> m Doc
+prettyCore :: (Carrier sig m, Member (Reader N) sig, Member (Writer (Set.Set N)) sig, Monad f) => (f (m Prec) -> m Prec) -> Core f (m Prec) -> m Prec
 prettyCore go = \case
   Lam b -> do
-    (n, b') <- withPrec 0 (binding go pretty b)
-    prec 0 (pretty (cyan backslash) <+> pretty n </> cyan dot <+> b')
+    (n, b') <- binding go pretty b
+    pure (prec 0 (pretty (cyan backslash) <+> pretty n </> cyan dot <+> withPrec 0 b'))
   f :$ a -> do
-    f' <- withPrec 10 (go f)
-    a' <- withPrec 11 (go a)
-    prec 10 (f' <+> a')
+    f' <- withPrec 10 <$> go f
+    a' <- withPrec 11 <$> go a
+    pure (prec 10 (f' <+> a'))
   Let v b -> do
-    v' <- withPrec 0 (go v)
-    (n, b') <- withPrec 0 (binding go prettyMeta b)
-    prec 0 (magenta (pretty "let") <+> pretty (n := v') </> magenta dot <+> b')
-  Type -> pure (yellow (pretty "Type"))
+    v' <- withPrec 0 <$> go v
+    (n, b') <- binding go prettyMeta b
+    pure (prec 0 (magenta (pretty "let") <+> pretty (n := v') </> magenta dot <+> withPrec 0 b'))
+  Type -> pure (atom (yellow (pretty "Type")))
   Pi t b -> do
-    t' <- withPrec 1 (go t)
-    (fvs, (n, b')) <- listen (withPrec 0 (binding go pretty b))
+    t' <- withPrec 1 <$> go t
+    (fvs, (n, b')) <- listen (binding go pretty b)
     let t'' | n `Set.member` fvs = parens (pretty (n ::: t'))
             | otherwise          = t'
-    prec 0 (t'' </> arrow <+> b')
+    pure (prec 0 (t'' </> arrow <+> withPrec 0 b'))
   where arrow = blue (pretty "→")
 
-prettyProblem :: (Carrier sig m, Member (Reader N) sig, Member (Reader P) sig, Member (Writer (Set.Set N)) sig, Monad f) => (f (m Doc) -> m Doc) -> Problem f (m Doc) -> m Doc
+prettyProblem :: (Carrier sig m, Member (Reader N) sig, Member (Writer (Set.Set N)) sig, Monad f) => (f (m Prec) -> m Prec) -> Problem f (m Prec) -> m Prec
 prettyProblem go = \case
   Ex t b -> do
-    t' <- withPrec 1 (go t)
-    (n, b') <- withPrec 0 (binding go prettyMeta b)
-    prec 0 (magenta (pretty "∃") <+> pretty (n ::: t') </> magenta dot <+> b')
+    t' <- withPrec 1 <$> go t
+    (n, b') <- binding go prettyMeta b
+    pure (prec 0 (magenta (pretty "∃") <+> pretty (n ::: t') </> magenta dot <+> withPrec 0 b'))
   p1 :===: p2 -> do
-    p1' <- withPrec 1 (go p1)
-    p2' <- withPrec 1 (go p2)
-    prec 0 (flatAlt (p1' <+> eq' <+> p2') (align (space <+> p1' </> eq' <+> p2')))
+    p1' <- withPrec 1 <$> go p1
+    p2' <- withPrec 1 <$> go p2
+    pure (prec 0 (flatAlt (p1' <+> eq' <+> p2') (align (space <+> p1' </> eq' <+> p2'))))
   where eq' = magenta (pretty "≡")
 
 
