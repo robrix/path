a28df46e248520c7b1a35cd44a150c37edc86d94
Rob Rix <rob.rix@me.com>
2019-08-03 11:02:46 -0400

    :fire: a redundant call to pretty.

--- src/Path/Pretty.hs
+++ src/Path/Pretty.hs
@@ -63,7 +63,7 @@ data Notice = Notice
 
 prettyNotice :: Notice -> Doc
 prettyNotice (Notice level (Excerpt path line span) reason context) = vsep
-  ( nest 2 (group (vsep [bold (pretty path) <> colon <> bold (pretty (succ (posLine (spanStart span)))) <> colon <> bold (pretty (succ (posColumn (spanStart span)))) <> colon <> maybe mempty ((space <>) . (<> colon) . prettyLevel) level, pretty reason]))
+  ( nest 2 (group (vsep [bold (pretty path) <> colon <> bold (pretty (succ (posLine (spanStart span)))) <> colon <> bold (pretty (succ (posColumn (spanStart span)))) <> colon <> maybe mempty ((space <>) . (<> colon) . prettyLevel) level, reason]))
   : blue (pretty (succ (posLine (spanStart span)))) <+> align (fold
     [ blue (pretty '|') <+> pretty line <> if "\n" `isSuffixOf` line then mempty else blue (pretty "<EOF>") <> hardline
     , blue (pretty '|') <+> caret span
