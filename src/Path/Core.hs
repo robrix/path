255fa47b2158d218f89e3e3abf9d91826b490989
Rob Rix <rob.rix@me.com>
2019-08-10 11:05:40 -0400

    Sort imports correctly.

--- src/Path/Core.hs
+++ src/Path/Core.hs
@@ -6,8 +6,8 @@ import           Control.Effect.Carrier
 import           Control.Monad.Module
 import qualified Data.Set as Set
 import           GHC.Generics (Generic1)
-import           Path.Pretty
 import           Path.Nat
+import           Path.Pretty
 import           Path.Scope
 import           Path.Syntax
 import           Path.Term
