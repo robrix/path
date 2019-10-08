5bb8f40d60ce4d7a2a974a89168b728c72a4825c
Rob Rix <rob.rix@me.com>
2019-10-08 01:32:59 -0400

    :fire: match.

--- src/Path/Scope.hs
+++ src/Path/Scope.hs
@@ -8,10 +8,6 @@ import Path.Nat
 import Syntax.Scope
 import Syntax.Var
 
-match :: Applicative f => (b -> Var a c) -> b -> Var a (f c)
-match f x = unVar B (F . pure) (f x)
-
-
 strengthen :: Functor f => f (Var (Fin 'Z) a) -> f a
 strengthen = fmap (unVar absurdFin id)
 
@@ -20,4 +16,4 @@ fromScopeFin :: Monad f => Scope () f (Var (Fin n) b) -> f (Var (Fin ('S n)) b)
 fromScopeFin = unScope >=> unVar (const (pure (B FZ))) (fmap (first FS))
 
 toScopeFin :: Applicative f => f (Var (Fin ('S n)) b) -> Scope () f (Var (Fin n) b)
-toScopeFin = Scope . fmap (match (unVar (maybe (B ()) (F . B) . strengthenFin) (F . F)))
+toScopeFin = Scope . fmap (unVar (maybe (B ()) (F . pure . B) . strengthenFin) (F . pure . F))
