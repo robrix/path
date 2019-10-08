92c632f5e2e29e8e8854a75f97caede02282a9c1
Rob Rix <rob.rix@me.com>
2019-10-08 01:29:40 -0400

    :fire: matchM.

--- src/Path/Scope.hs
+++ src/Path/Scope.hs
@@ -11,9 +11,6 @@ import Syntax.Var
 match :: Applicative f => (b -> Var a c) -> b -> Var a (f c)
 match f x = unVar B (F . pure) (f x)
 
-matchM :: (Applicative f, Functor m) => (b -> m (Var a c)) -> b -> m (Var a (f c))
-matchM f x = unVar B (F . pure) <$> f x
-
 
 strengthen :: Functor f => f (Var (Fin 'Z) a) -> f a
 strengthen = fmap (unVar absurdFin id)
