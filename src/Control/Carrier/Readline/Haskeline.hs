c64f72c108d7b4dbe14c331b6209e422616cdf08
Rob Rix <rob.rix@me.com>
2019-10-08 02:09:09 -0400

    Move the Readline carrier into its own module.

--- src/Control/Carrier/Readline/Haskeline.hs
+++ src/Control/Carrier/Readline/Haskeline.hs
@@ -1,2 +1,58 @@
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, RankNTypes, TypeOperators, UndecidableInstances #-}
 module Control.Carrier.Readline.Haskeline
-() where
+( -- * Readline effect
+  module Control.Effect.Readline
+  -- * Readline carrier
+, runReadline
+, ReadlineC(..)
+  -- * MonadException
+, runControlIO
+, ControlIOC(..)
+  -- * Re-exports
+, Carrier
+) where
+
+import Control.Effect.Carrier
+import Control.Effect.Lift
+import Control.Effect.Reader
+import Control.Effect.Readline
+import Control.Monad.Fix
+import Control.Monad.IO.Class
+import Control.Monad.Trans
+import Path.Pretty
+import System.Console.Haskeline hiding (Handler, handle)
+
+runReadline :: MonadException m => Prefs -> Settings m -> ReadlineC m a -> m a
+runReadline prefs settings = runInputTWithPrefs prefs settings . runM . runReader (Line 0) . runReadlineC
+
+newtype ReadlineC m a = ReadlineC { runReadlineC :: ReaderC Line (LiftC (InputT m)) a }
+  deriving (Applicative, Functor, Monad, MonadFix, MonadIO)
+
+instance (MonadException m, MonadIO m) => Carrier (Readline :+: Lift (InputT m)) (ReadlineC m) where
+  eff (L (Prompt prompt k)) = ReadlineC $ do
+    str <- lift (lift (getInputLine (cyan <> prompt <> plain)))
+    local increment (runReadlineC (k str))
+    where cyan = "\ESC[1;36m\STX"
+          plain = "\ESC[0m\STX"
+  eff (L (Print text k)) = putDoc text *> k
+  eff (L (AskLine k)) = ReadlineC ask >>= k
+  eff (R other) = ReadlineC (eff (R (handleCoercible other)))
+
+runControlIO :: (forall x . m x -> IO x) -> ControlIOC m a -> m a
+runControlIO handler = runReader (Handler handler) . runControlIOC
+
+newtype ControlIOC m a = ControlIOC { runControlIOC :: ReaderC (Handler m) m a }
+  deriving (Applicative, Functor, Monad, MonadFix, MonadIO)
+
+newtype Handler m = Handler (forall x . m x -> IO x)
+
+runHandler :: Handler m -> ControlIOC m a -> IO a
+runHandler h@(Handler handler) = handler . runReader h . runControlIOC
+
+instance Carrier sig m => Carrier sig (ControlIOC m) where
+  eff op = ControlIOC (eff (R (handleCoercible op)))
+
+instance (Carrier sig m, MonadIO m) => MonadException (ControlIOC m) where
+  controlIO f = ControlIOC $ do
+    handler <- ask
+    liftIO (f (RunIO (fmap pure . runHandler handler)) >>= runHandler handler)
--- src/Path/REPL.hs
+++ src/Path/REPL.hs
@@ -1,17 +1,15 @@
 {-# LANGUAGE FlexibleContexts, FlexibleInstances, GeneralizedNewtypeDeriving, KindSignatures, LambdaCase, MultiParamTypeClasses, RankNTypes, TypeApplications, TypeOperators, UndecidableInstances #-}
 module Path.REPL where
 
+import Control.Carrier.Readline.Haskeline
 import Control.Effect.Carrier
 import Control.Effect.Error
 import Control.Effect.Lift
 import Control.Effect.Reader
-import Control.Effect.Readline
 import Control.Effect.State
 import Control.Effect.Writer
 import Control.Monad (foldM, join, unless, void)
-import Control.Monad.Fix
 import Control.Monad.IO.Class
-import Control.Monad.Trans (MonadTrans(..))
 import Data.Foldable (for_)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
@@ -40,41 +38,6 @@ import Syntax.Var
 import System.Console.Haskeline hiding (Handler, handle)
 import System.Directory (createDirectoryIfMissing, getHomeDirectory)
 
-runREPL :: MonadException m => Prefs -> Settings m -> REPLC m a -> m a
-runREPL prefs settings = runInputTWithPrefs prefs settings . runM . runReader (Line 0) . runREPLC
-
-newtype REPLC m a = REPLC { runREPLC :: ReaderC Line (LiftC (InputT m)) a }
-  deriving (Applicative, Functor, Monad, MonadFix, MonadIO)
-
-instance (MonadException m, MonadIO m) => Carrier (Readline :+: Lift (InputT m)) (REPLC m) where
-  eff (L (Prompt prompt k)) = REPLC $ do
-    str <- lift (lift (getInputLine (cyan <> prompt <> plain)))
-    local increment (runREPLC (k str))
-    where cyan = "\ESC[1;36m\STX"
-          plain = "\ESC[0m\STX"
-  eff (L (Print text k)) = putDoc text *> k
-  eff (L (AskLine k)) = REPLC ask >>= k
-  eff (R other) = REPLC (eff (R (handleCoercible other)))
-
-runControlIO :: (forall x . m x -> IO x) -> ControlIOC m a -> m a
-runControlIO handler = runReader (Handler handler) . runControlIOC
-
-newtype ControlIOC m a = ControlIOC { runControlIOC :: ReaderC (Handler m) m a }
-  deriving (Applicative, Functor, Monad, MonadFix, MonadIO)
-
-newtype Handler m = Handler (forall x . m x -> IO x)
-
-runHandler :: Handler m -> ControlIOC m a -> IO a
-runHandler h@(Handler handler) = handler . runReader h . runControlIOC
-
-instance Carrier sig m => Carrier sig (ControlIOC m) where
-  eff op = ControlIOC (eff (R (handleCoercible op)))
-
-instance (Carrier sig m, MonadIO m) => MonadException (ControlIOC m) where
-  controlIO f = ControlIOC $ do
-    handler <- ask
-    liftIO (f (RunIO (fmap pure . runHandler handler)) >>= runHandler handler)
-
 repl :: MonadIO m => [FilePath] -> m ()
 repl packageSources = liftIO $ do
   homeDir <- getHomeDirectory
@@ -87,7 +50,7 @@ repl packageSources = liftIO $ do
         }
   createDirectoryIfMissing True settingsDir
   runM (runControlIO runM
-       (runREPL prefs settings
+       (runReadline prefs settings
        (script packageSources)))
 
 script :: ( Carrier sig m
